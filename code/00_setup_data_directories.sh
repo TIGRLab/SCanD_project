051d86c569e41f20f89eaae9d8f4d0e08f7953bf
Erin W Dickie <erin.w.dickie@gmail.com>
2022-09-23 15:37:52 -0400

    adding the anat running bit

--- code/00_setup_data_directories.sh
+++ code/00_setup_data_directories.sh
@@ -1,12 +1,23 @@
 ## hold many of the scripts needed to set-up the repo for the first time..
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+CURRENT_DIR=${PWD}
 
+cd ${SCRIPT_DIR}
 ## i.e. makes folders and links to software in the arisvoin (CAMH) lab space
-mkdir -p ${SCRIPT_DIR}/../containers
-mkdir -p ${SCRIPT_DIR}/../data
-mkdir -p ${SCRIPT_DIR}/../data/local
-mkdir -p ${SCRIPT_DIR}/../data/share
-mkdir -p ${SCRIPT_DIR}/../templates
-mkdir -p ${SCRIPT_DIR}/../logs
+echo "making directories"
+mkdir -p containers
+mkdir -p data
+mkdir -p data/local
+mkdir -p data/share
+mkdir -p templates
+mkdir -p templates/.cache
+mkdir -p logs
 
+## link the containers
+echo "linking singularity containers"
+ln -s /project/a/arisvoin/edickie/containers/fmriprep-21.0.2.simg containers/fmriprep-21.0.2.simg
+ln -s /project/a/arisvoin/edickie/containers/fmriprep_ciftity-v1.3.2-2.3.3.simg containers/fmriprep_ciftity-v1.3.2-2.3.3.simg
 
+## use the fmriprep containers to download templateflow templates
+echo "copying templates..this might take a bit"
+scp -r /project/a/arisvoin/edickie/templateflow templates/.cache/templateflow
--- code/01_fmriprep_anat_scinet.sh
+++ code/01_fmriprep_anat_scinet.sh
@@ -12,11 +12,8 @@ export THREADS_PER_COMMAND=2
 
 ####----### the next bit only works IF this script is submitted from the $BASEDIR/$OPENNEURO_DS folder...
 
-## set OPENNEURO_DSID to the openneuro dataset id
-OPENNEURO_DSID=`basename ${SLURM_SUBMIT_DIR}`
-
 ## set the second environment variable to get the base directory
-BASEDIR=`dirname ${SLURM_SUBMIT_DIR}`
+BASEDIR=${SLURM_SUBMIT_DIR}
 
 ## set up a trap that will clear the ramdisk if it is not cleared
 function cleanup_ramdisk {
@@ -32,29 +29,29 @@ function cleanup_ramdisk {
 trap "cleanup_ramdisk" TERM
 
 # input is BIDS_DIR this is where the data downloaded from openneuro went
-export BIDS_DIR=${BASEDIR}/${OPENNEURO_DSID}/bids
+export BIDS_DIR=${BASEDIR}/data/local/bids
 
 ## these folders envs need to be set up for this script to run properly 
 ## see notebooks/00_setting_up_envs.md for the set up instructions
-export FMRIPREP_HOME=${BASEDIR}/fmriprep_home
-export SING_CONTAINER=${BASEDIR}/containers/fmriprep-20.2.7.simg
+export FMRIPREP_HOME=${BASEDIR}/templates
+export SING_CONTAINER=${BASEDIR}/containers/fmriprep-21.0.2.simg
 
 
 ## setting up the output folders
-export OUTPUT_DIR=${BASEDIR}/${OPENNEURO_DSID}/derived/
+export OUTPUT_DIR=${BASEDIR}/data/local/
 # export LOCAL_FREESURFER_DIR=${SCRATCH}/${STUDY}/data/derived/freesurfer-6.0.1
-export WORK_DIR=${BBUFFER}/${STUDY}/fmriprep
-export LOGS_DIR=${BASEDIR}/${OPENNEURO_DSID}/logs
-mkdir -vp ${OUTPUT_DIR} ${WORK_DIR} ${LOGS_DIR} # ${LOCAL_FREESURFER_DIR}
+export WORK_DIR=${BBUFFER}/SCanD/fmriprep
+export LOGS_DIR=${BASEDIR}/logs
+mkdir -vp ${OUTPUT_DIR} ${WORK_DIR} # ${LOCAL_FREESURFER_DIR}
 
 ## get the subject list from a combo of the array id, the participants.tsv and the chunk size
 bigger_bit=`echo "($SLURM_ARRAY_TASK_ID + 1) * ${SUB_SIZE}" | bc`
 SUBJECTS=`sed -n -E "s/sub-(\S*)\>.*/\1/gp" ${BIDS_DIR}/participants.tsv | head -n ${bigger_bit} | tail -n ${SUB_SIZE}`
 
 ## set singularity environment variables that will point to the freesurfer license and the templateflow bits
-export SINGULARITYENV_TEMPLATEFLOW_HOME=/home/fmriprep/.cache/templateflow
+export SINGULARITYENV_TEMPLATEFLOW_HOME=${BASEDIR}/templates/templateflow
 # Make sure FS_LICENSE is defined in the container.
-export SINGULARITYENV_FS_LICENSE=/home/fmriprep/.freesurfer.txt
+export SINGULARITYENV_FS_LICENSE=${BASEDIR}/.freesurfer.txt
 
 # # Remove IsRunning files from FreeSurfer
 # for subject in $SUBJECTS: do
@@ -63,7 +60,7 @@ export SINGULARITYENV_FS_LICENSE=/home/fmriprep/.freesurfer.txt
 
 
 singularity run --cleanenv \
-    -B ${BASEDIR}/fmriprep_home:/home/fmriprep --home /home/fmriprep \
+    -B ${BASEDIR}/templates:/home/fmriprep --home /home/fmriprep \
     -B ${BIDS_DIR}:/bids \
     -B ${OUTPUT_DIR}:/derived \
     -B ${WORK_DIR}:/work \
@@ -77,9 +74,7 @@ singularity run --cleanenv \
     --low-mem \
     --mem-mb 12000 \
     --output-space T1w MNI152NLin2009cAsym \
-    --use-aroma \
     --notrack \
-    --cifti-output 91k \
     --anat-only 
 
 exitcode=$?
@@ -93,5 +88,3 @@ for subject in $SUBJECTS; do
 echo "sub-$subject   ${SLURM_ARRAY_TASK_ID}    $exitcode" \
       >> ${LOGS_DIR}/${SLURM_JOB_NAME}.${SLURM_ARRAY_JOB_ID}.tsv
 done
-echo Finished tasks ${SLURM_ARRAY_TASK_ID} with exit code $exitcode
-exit $exitcode
