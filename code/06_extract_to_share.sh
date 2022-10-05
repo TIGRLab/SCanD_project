221ebd9a295c09510f38b7826421420d5b448210
Erin Dickie <Erin.Dickie@camh.ca>
2022-10-05 17:00:40 -0400

    updating extract script

--- code/06_extract_to_share.sh
+++ code/06_extract_to_share.sh
@@ -28,13 +28,31 @@ singularity run \
     ${PROJECT_DIR}/containers/fmriprep_ciftity-v1.3.2-2.3.3.simg \
       /data/bids /data group 
 
+## copy over the ciftify QC outputs
 rsync -a ${PROJECT_DIR}/data/local/ciftify/qc_recon_all  ${PROJECT_DIR}/data/share/ciftify/
 rsync -a ${PROJECT_DIR}/data/local/ciftify/qc_fmri  ${PROJECT_DIR}/data/share/ciftify/
+
+## also run ciftify group step - on the cleaned RSN maps
+
+singularity exec \
+  -B ${PROJECT_DIR}/data/local:/derived \
+  ${PROJECT_DIR}/containers/fmriprep_ciftity-v1.3.2-2.3.3.simg \
+  cifti_vis_RSN index \
+  --qcdir /derived/cifti_clean/qc_rsn \
+  --ciftify-work-dir /derived/ciftify 
+
+## copy over the ciftify QC outputs
+echo "copying over the resting state images from cleaned images"
+rsync -a ${PROJECT_DIR}/data/local/cifti_clean/qc_rsn ${PROJECT_DIR}/data/share/ciftify/cifti_clean
 
 ## also run freesurfer ENIGMA scripts
 
 ## copy over the freesurfer results
-## copy over the ciftify QC outputs
+
 ## copy over the parcellated files
+echo "copying over the parcellated files"
+rsync -a ${PROJECT_DIR}/data/local/parcellated ${PROJECT_DIR}/data/share/
 
 ## create a spreadsheet output for wether or not outputs were found (each step) for each functional file
+
+## zip all the outputs for transfer
