26f66f075f29683f6ac35f96c04c3164c587217d
GhazalehManj <126309136+GhazalehManj@users.noreply.github.com>
2024-01-18 15:22:54 -0500

    Update 03_xcp_scinet.sh

--- code/03_xcp_scinet.sh
+++ code/03_xcp_scinet.sh
@@ -1,1 +1,69 @@
+#!/bin/bash
+#SBATCH --job-name=xcp
+#SBATCH --output=logs/%x_%j.out
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=40
+#SBATCH --time=12:00:00
 
+
+SUB_SIZE=2 ## number of subjects to run is 1 because there are multiple tasks/run that will run in parallel
+CORES=40
+export THREADS_PER_COMMAND=2
+
+####----### the next bit only works IF this script is submitted from the $BASEDIR/$OPENNEURO_DS folder...
+
+## set the second environment variable to get the base directory
+BASEDIR=${SLURM_SUBMIT_DIR}
+
+## set up a trap that will clear the ramdisk if it is not cleared
+function cleanup_ramdisk {
+    echo -n "Cleaning up ramdisk directory /$SLURM_TMPDIR/ on "
+    date
+    rm -rf /$SLURM_TMPDIR
+    echo -n "done at "
+    date
+}
+
+#trap the termination signal, and call the function 'trap_term' when
+# that happens, so results may be saved.
+trap "cleanup_ramdisk" TERM
+
+export SING_CONTAINER=${BASEDIR}/containers/xcp_d.simg
+
+
+## setting up the output folders
+export OUTPUT_DIR=${BASEDIR}/data/local/
+export FMRI_DIR=${BASEDIR}/data/local/fmriprep/
+
+export WORK_DIR=${BBUFFER}/work/xcp
+export LOGS_DIR=${BASEDIR}/logs
+mkdir -vp ${OUTPUT_DIR} ${WORK_DIR}
+
+## get the subject list from a combo of the array id, the participants.tsv and the chunk size
+bigger_bit=`echo "($SLURM_ARRAY_TASK_ID + 1) * ${SUB_SIZE}" | bc`
+SUBJECTS=`sed -n -E "s/sub-(\S*)\>.*/\1/gp" ${BIDS_DIR}/participants.tsv | head -n ${bigger_bit} | tail -n ${SUB_SIZE}`
+
+
+
+
+singularity run --cleanenv  ${SING_CONTAINER} \
+    $FMRI_DIR\
+    $OUTPUT_DIR\
+    participant\
+    --participant_label ${SUBJECTS} \
+    -w ${WORK_DIR} \
+    --omp-nthreads 8 \
+    --nthreads 40 \
+    --mem-gb 15 \
+    --notrack
+
+# note, if you have top-up fieldmaps than you can uncomment the last two lines of the above script
+
+exitcode=$?
+
+
+# Output results to a table
+for subject in $SUBJECTS; do
+echo "sub-$subject   ${SLURM_ARRAY_TASK_ID}    $exitcode" \
+      >> ${LOGS_DIR}/${SLURM_JOB_NAME}.${SLURM_ARRAY_JOB_ID}.tsv
+done
