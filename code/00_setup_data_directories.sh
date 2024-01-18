bce177835a8a3e7c286574fa62c437e67d080086
GhazalehManj <126309136+GhazalehManj@users.noreply.github.com>
2024-01-18 14:25:32 -0500

    Update 00_setup_data_directories.sh

--- code/00_setup_data_directories.sh
+++ code/00_setup_data_directories.sh
@@ -22,6 +22,7 @@ ln -s ${CONTAINER_DIR}/fmriprep-20.2.7.simg containers/fmriprep-20.2.7.simg
 ln -s ${CONTAINER_DIR}/fmriprep_ciftity-v1.3.2-2.3.3.simg containers/fmriprep_ciftity-v1.3.2-2.3.3.simg
 ln -s ${CONTAINER_DIR}/mriqc-22.0.6.simg containers/mriqc-22.0.6.simg 
 ln -s ${CONTAINER_DIR}/qsiprep_0.16.0RC3.simg containers/qsiprep_0.16.0RC3.simg 
+ln -s ${CONTAINER_DIR}/xcp_d.simg containers/xcp_d.simg
 
 ## copy in Erin's freesurfer licence
 cp /scinet/course/ss2019/3/5_neuroimaging/fs_license/license.txt templates/.freesurfer.txt
