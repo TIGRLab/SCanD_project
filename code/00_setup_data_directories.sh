c35f9039b6063ed9c1bb636ab8fd84765a5be8a2
GhazalehManj <126309136+GhazalehManj@users.noreply.github.com>
2024-01-18 16:23:06 -0500

    Update 00_setup_data_directories.sh

--- code/00_setup_data_directories.sh
+++ code/00_setup_data_directories.sh
@@ -16,10 +16,7 @@ mkdir -p logs
 ## link the containers
 echo "linking singularity containers"
 CONTAINER_DIR=/scinet/course/ss2019/3/5_neuroimaging/containers
-#ln -s ${CONTAINER_DIR}/fmriprep-21.0.2.simg containers/fmriprep-21.0.2.simg
 ln -s ${CONTAINER_DIR}/fmriprep-20.2.7.simg containers/fmriprep-20.2.7.simg
-#ln -s ${CONTAINER_DIR}/fmriprep-20.1.1.simg containers/fmriprep-20.1.1.simg
-ln -s ${CONTAINER_DIR}/fmriprep_ciftity-v1.3.2-2.3.3.simg containers/fmriprep_ciftity-v1.3.2-2.3.3.simg
 ln -s ${CONTAINER_DIR}/mriqc-22.0.6.simg containers/mriqc-22.0.6.simg 
 ln -s ${CONTAINER_DIR}/qsiprep_0.16.0RC3.simg containers/qsiprep_0.16.0RC3.simg 
 ln -s ${CONTAINER_DIR}/xcp_d.simg containers/xcp_d.simg
