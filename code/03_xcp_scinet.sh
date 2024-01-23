742946219cc9c9d256fc8955e403651ca8c6b41b
GhazalehManj <126309136+GhazalehManj@users.noreply.github.com>
2024-01-22 21:48:16 -0500

    Update 03_xcp_scinet.sh

--- code/03_xcp_scinet.sh
+++ code/03_xcp_scinet.sh
@@ -3,7 +3,7 @@
 #SBATCH --output=logs/%x_%j.out
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=40
-#SBATCH --time=08:00:00
+#SBATCH --time=06:00:00
 
 
 SUB_SIZE=2 ## number of subjects to run is 1 because there are multiple tasks/run that will run in parallel
