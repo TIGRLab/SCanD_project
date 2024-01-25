0fd5d8be2a1390368eccc2336a14235b4c7f635e
GhazalehManj <126309136+GhazalehManj@users.noreply.github.com>
2024-01-24 20:00:23 -0500

    Update 03_xcp_scinet.sh

--- code/03_xcp_scinet.sh
+++ code/03_xcp_scinet.sh
@@ -55,7 +55,7 @@ singularity run --cleanenv  ${SING_CONTAINER} \
     --participant_label ${SUBJECTS} \
     -w ${WORK_DIR} \
     --cifti \
-    --smoothing 2 \
+    --smoothing 0 \
     --notrack
 
 # note, if you have top-up fieldmaps than you can uncomment the last two lines of the above script
