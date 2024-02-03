3c4a59b85d9c306225f002a18bfba56809fe63a5
GhazalehManj <126309136+GhazalehManj@users.noreply.github.com>
2024-02-02 23:21:49 -0500

    Update 01_fmriprep_anat_scinet.sh

--- code/01_fmriprep_anat_scinet.sh
+++ code/01_fmriprep_anat_scinet.sh
@@ -6,7 +6,7 @@
 #SBATCH --time=16:00:00
 
 
-SUB_SIZE=5 ## number of subjects to run
+SUB_SIZE=2 ## number of subjects to run
 CORES=40
 export THREADS_PER_COMMAND=2
 
@@ -46,12 +46,12 @@ export WORK_DIR=${BBUFFER}/SCanD/fmriprep
 export LOGS_DIR=${BASEDIR}/logs
 mkdir -vp ${OUTPUT_DIR} ${WORK_DIR} # ${LOCAL_FREESURFER_DIR}
 
-## get the subject list from a combo of the array id, the participants.tsv and the chunk size
-bigger_bit=`echo "($SLURM_ARRAY_TASK_ID + 1) * ${SUB_SIZE}" | bc`
+## get the subject list from a combo of the array id, the participants.tsv and the chunk 
+bigger_bit=`echo "($SLURM_ARRAY_TASK_ID + 1) * ${SUB_}" | bc`
 
 N_SUBJECTS=$(( $( wc -l ${BIDS_DIR}/participants.tsv | cut -f1 -d' ' ) - 1 ))
-array_job_length=$(echo "$N_SUBJECTS/${SUB_SIZE}" | bc)
-Tail=$((N_SUBJECTS-(array_job_length*SUB_SIZE)))
+array_job_length=$(echo "$N_SUBJECTS/${SUB_}" | bc)
+Tail=$((N_SUBJECTS-(array_job_length*SUB_)))
 
 if [ "$SLURM_ARRAY_TASK_ID" -eq "$array_job_length" ]; then
     SUBJECTS=`sed -n -E "s/sub-(\S*)\>.*/\1/gp" ${BIDS_DIR}/participants.tsv  | head -n ${N_SUBJECTS} | tail -n ${Tail}`
