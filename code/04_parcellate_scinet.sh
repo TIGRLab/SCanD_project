100d09ada357ae198a69a977da049b248f797da8
GhazalehManj <126309136+GhazalehManj@users.noreply.github.com>
2024-02-06 18:52:21 -0500

    Update 04_parcellate_scinet.sh

--- code/04_parcellate_scinet.sh
+++ code/04_parcellate_scinet.sh
@@ -14,8 +14,8 @@ module load gnu-parallel/20191122
 
 ## note the dlabel file path must be a relative to the output folder
 export parcellation_dir=${BASEDIR}/data/local/xcp_d
-export dlabel_file="pace-fsLR_atlas-Tian_den-91k_dseg.dlabel.nii"
-export atlas="Tian"
+export dlabel_file="space-fsLR_atlas-HCP_den-91k_dseg.dlabel.nii.dlabel.nii"
+export atlas= "HCP"
 
 ## set up a trap that will clear the ramdisk if it is not cleared
 function cleanup_ramdisk {
