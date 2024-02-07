96f4c57e955154f2d0478087973926a2419237cf
GhazalehManj <126309136+GhazalehManj@users.noreply.github.com>
2024-02-07 15:34:20 -0500

    Update stage_2.sh

--- stage_2.sh
+++ stage_2.sh
@@ -1,1 +1,26 @@
+## note step one is to make sure you are on one of the login nodes
+ssh niagara.scinet.utoronto.ca
 
+## go to the repo and pull new changes
+cd ${SCRATCH}/SCanD_project_GMANJ
+git pull
+
+##fmriprep_func
+## figuring out appropriate array-job size
+SUB_SIZE=2 # for func the sub size is moving to 1 participant because there are two runs and 8 tasks per run..
+N_SUBJECTS=$(( $( wc -l ./data/local/bids/participants.tsv | cut -f1 -d' ' ) - 1 ))
+array_job_length=$(echo "$N_SUBJECTS/${SUB_SIZE}" | bc)
+echo "number of array is: ${array_job_length}"
+
+## submit the array job to the queue
+sbatch --array=0-${array_job_length} ./code/02_fmriprep_func_scinet.sh
+
+##enigma_dti
+## figuring out appropriate array-job size
+SUB_SIZE=2 # for func the sub size is moving to 1 participant because there are two runs and 8 tasks per run..
+N_SUBJECTS=$(( $( wc -l ./data/local/bids/participants.tsv | cut -f1 -d' ' ) - 1 ))
+array_job_length=$(echo "$N_SUBJECTS/${SUB_SIZE}" | bc)
+echo "number of array is: ${array_job_length}"
+
+## submit the array job to the queue
+sbatch --array=0-${array_job_length} ./code/02_enigma_dti_scinet.sh
