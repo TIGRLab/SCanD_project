a853a29f6cf8c1510818fc6152c533aa3f3f5d32
GhazalehManj <126309136+GhazalehManj@users.noreply.github.com>
2024-02-07 15:36:00 -0500

    Update stage_4.sh

--- stage_4.sh
+++ stage_4.sh
@@ -1,1 +1,14 @@
+## note step one is to make sure you are on one of the login nodes
+ssh niagara.scinet.utoronto.ca
 
+## go to the repo and pull new changes
+cd ${SCRATCH}/SCanD_project_GMANJ
+git pull
+
+SUB_SIZE=10 # for func the sub size is moving to 1 participant because there are two runs and 8 tasks per run..
+N_DTSERIES=$(ls -1d ./data/local/ciftify/sub*/MNINonLinear/Results/*task*/*dtseries.nii | wc -l)
+array_job_length=$(echo "$N_DTSERIES/${SUB_SIZE}" | bc)
+echo "number of array is: ${array_job_length}"
+
+## submit the array job to the queue
+sbatch --array=0-${array_job_length} ./code/04_parcellate_scinet.sh
