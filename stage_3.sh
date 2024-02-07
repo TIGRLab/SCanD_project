eebe187e4e71ce4db93311267d0423a59fbdb877
GhazalehManj <126309136+GhazalehManj@users.noreply.github.com>
2024-02-07 15:35:05 -0500

    Update stage_3.sh

--- stage_3.sh
+++ stage_3.sh
@@ -1,1 +1,29 @@
+## note step one is to make sure you are on one of the login nodes
+ssh niagara.scinet.utoronto.ca
 
+## go to the repo and pull new changes
+cd ${SCRATCH}/SCanD_project_GMANJ
+git pull
+
+##ciftify_anat
+## figuring out appropriate array-job size
+SUB_SIZE=8 # for func the sub size is moving to 1 participant because there are two runs and 8 tasks per run..
+N_SUBJECTS=$(( $( wc -l ./data/local/bids/participants.tsv | cut -f1 -d' ' ) - 1 ))
+array_job_length=$(echo "$N_SUBJECTS/${SUB_SIZE}" | bc)
+echo "number of array is: ${array_job_length}"
+
+## submit the array job to the queue
+sbatch --array=0-${array_job_length} ./code/03_ciftify_anat_scinet.sh
+
+##xcp_d
+## figuring out appropriate array-job size
+SUB_SIZE=1 # for func the sub size is moving to 1 participant because there are two runs and 8 tasks per run..
+N_SUBJECTS=$(( $( wc -l ./data/local/bids/participants.tsv | cut -f1 -d' ' ) - 1 ))
+array_job_length=$(echo "$N_SUBJECTS/${SUB_SIZE}" | bc)
+echo "number of array is: ${array_job_length}"
+
+## submit the array job to the queue
+sbatch --array=0-${array_job_length} ./code/03_xcp_scinet.sh
+
+##enigma_extract
+source ./code/03_ENIGMA_ExtractCortical.sh
