feed89bf2592caf7a685b61b4142993bb7dc27a0
GhazalehManj <126309136+GhazalehManj@users.noreply.github.com>
2024-02-13 15:54:30 -0500

    Update 02_qsirecon_scinet.sh

--- code/02_qsirecon_scinet.sh
+++ code/02_qsirecon_scinet.sh
@@ -62,24 +62,32 @@ fi
 export SINGULARITYENV_FS_LICENSE=/home/qsiprep/.freesurfer.txt
 
 
-singularity run --cleanenv \
-    -B ${BASEDIR}/templates:/home/qsiprep --home /home/qsiprep \
-    -B ${BIDS_DIR}:/bids \
-    -B ${QSIPREP_DIR}:/derived \
-    -B ${WORK_DIR}:/work \
-    -B ${OUTPUT_DIR}:/out \
-    ${SING_CONTAINER} \
-    /bids /out participant \
-    --skip-bids-validation \
-    --participant_label ${SUBJECTS} \
-    -w /work \
-    --skip-bids-validation \
-    --omp-nthreads 8 \
-    --nthreads 40 \
-    --recon-only \
-    --recon-spec reorient_fslstd \
-    --recon-input /derived \
-    --output-resolution 2.0 
+
+
+
+QSIRECON_OUT=${OUTPUT_DIR}/qsirecon/sub-${SUBJECTS}/ses-01/dwi/sub-${SUBJECTS}_ses-01_acq-singleshelldir60b1000_run-1_space-T1w_desc-preproc_fslstd
+DTIFIT_OUT=${OUTPUT_DIR}/dtifit/sub-${SUBJECTS}/ses-01/dwi/sub-${SUBJECTS}_ses-01_acq-singleshelldir60b1000_run-1_space-T1w_desc-preproc_fslstd
+DTIFIT_dir=$(dirname ${DTIFIT_OUT})
+DTIFIT_name=$(basename ${DTIFIT_OUT})
+
+mkdir -p $DTIFIT_dir
+
+
+singularity exec \
+  -H ${TMP_DIR} \
+  -B ${QSIRECON_OUT}_dwi.nii.gz \
+  -B ${QSIRECON_OUT}_mask.nii.gz \
+  -B ${QSIRECON_OUT}_dwi.bvec \
+  -B ${QSIRECON_OUT}_dwi.bval \
+  -B ${DTIFIT_dir}:/out \
+  ${SING_CONTAINER} \
+  dtifit -k ${QSIRECON_OUT}_dwi.nii.gz \
+  -m ${QSIRECON_OUT}_mask.nii.gz \
+  -r ${QSIRECON_OUT}_dwi.bvec \
+  -b ${QSIRECON_OUT}_dwi.bval \
+  --save_tensor --sse \
+  -o /out/$DTIFIT_name
+
 
  exitcode=$?
 
@@ -93,3 +101,6 @@ for subject in $SUBJECTS; do
             >> ${LOGS_DIR}/${SLURM_JOB_NAME}.${SLURM_ARRAY_JOB_ID}.tsv
     fi
 done
+
+
+
