364889aa96f4511fa6065350ec4fcbc8f772629a
GhazalehManj <126309136+GhazalehManj@users.noreply.github.com>
2024-02-13 12:17:37 -0500

    Update 02_enigma_dti_scinet.sh

--- code/02_enigma_dti_scinet.sh
+++ code/02_enigma_dti_scinet.sh
@@ -1,96 +1,38 @@
 #!/bin/bash
-#SBATCH --job-name=qsiprep
+#SBATCH --job-name=enigma
 #SBATCH --output=logs/%x_%j.out 
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=40
-#SBATCH --time=06:00:00
+#SBATCH --time=10:00:00
 
+# Built from https://hub.docker.com/repository/docker/salimmansour/tbss/general
 
-SUB_SIZE=2 ## number of subjects to run is 1 because there are multiple tasks/run that will run in parallel 
-CORES=40
-export THREADS_PER_COMMAND=2
+DTIFIT_DIR=OUTPUT_DIR=${BASEDIR}/data/local/dtifit
+ENIGMA_DIR=OUTPUT_DIR=${BASEDIR}/data/local/enigmaDTI
+TBSS_CONTAINER=/scratch/a/arisvoin/smansour/SCanD_SPINS/containers/tbss2.simg
+ENIGMA_SCRIPT=/project/a/arisvoin/smansour/SPINS/SPINS_enigmaDTI/code/enigma_group.sh
 
-####----### the next bit only works IF this script is submitted from the $BASEDIR/$OPENNEURO_DS folder...
+singularity exec -H /project/a/arisvoin/smansour/SPASD/SPASD_enigmaDTI/tmp \
+  -B $PROJECT/ENIGMA_DTI_BIDS \
+  -B /project/a/arisvoin/smansour/SPASD/SPASD_enigmaDTI/data/enigmaDTI:/enigma_dir \
+  -B /project/a/arisvoin/smansour/SPASD/SPASD_enigmaDTI/data/dtifit:/dtifit_dir \
+  /scratch/a/arisvoin/smansour/SCanD_SPINS/containers/tbss2.simg \
+  /bin/bash
 
-## set the second environment variable to get the base directory
-BASEDIR=${SLURM_SUBMIT_DIR}
+DTIFIT_DIR=/dtifit_dir
+OUT_DIR=/enigma_dir
 
-## set up a trap that will clear the ramdisk if it is not cleared
-function cleanup_ramdisk {
-    echo -n "Cleaning up ramdisk directory /$SLURM_TMPDIR/ on "
-    date
-    rm -rf /$SLURM_TMPDIR
-    echo -n "done at "
-    date
-}
+# modify this to the location you cloned the repo to
+ENIGMA_DTI_BIDS=/opt/ENIGMA_DTI_BIDS
+ENIGMA_DTI_BIDS=$PROJECT/ENIGMA_DTI_BIDS
 
-#trap the termination signal, and call the function 'trap_term' when
-# that happens, so results may be saved.
-trap "cleanup_ramdisk" TERM
-
-# input is BIDS_DIR this is where the data downloaded from openneuro went
-export BIDS_DIR=${BASEDIR}/data/local/bids
-
-## these folders envs need to be set up for this script to run properly 
-## see notebooks/00_setting_up_envs.md for the set up instructions
-export QSIPREP_HOME=${BASEDIR}/templates
-export SING_CONTAINER=${BASEDIR}/containers/qsiprep_0.16.0RC3.simg
-
-## setting up the output folders
-# export OUTPUT_DIR=${BASEDIR}/data/local/fmriprep  # use if version of fmriprep >=20.2
-export OUTPUT_DIR=${BASEDIR}/data/local/ # use if version of fmriprep <=20.1
-
-# export LOCAL_FREESURFER_DIR=${SCRATCH}/${STUDY}/data/derived/freesurfer-6.0.1
-export WORK_DIR=${BBUFFER}/SCanD/qsiprep
-export LOGS_DIR=${BASEDIR}/logs
-mkdir -vp ${OUTPUT_DIR} ${WORK_DIR} # ${LOCAL_FREESURFER_DIR}
-
-bigger_bit=`echo "($SLURM_ARRAY_TASK_ID + 1) * ${SUB_SIZE}" | bc`
-
-N_SUBJECTS=$(( $( wc -l ${BIDS_DIR}/participants.tsv | cut -f1 -d' ' ) - 1 ))
-array_job_length=$(echo "$N_SUBJECTS/${SUB_SIZE}" | bc)
-Tail=$((N_SUBJECTS-(array_job_length*SUB_SIZE)))
-
-if [ "$SLURM_ARRAY_TASK_ID" -eq "$array_job_length" ]; then
-    SUBJECTS=`sed -n -E "s/sub-(\S*)\>.*/\1/gp" ${BIDS_DIR}/participants.tsv  | head -n ${N_SUBJECTS} | tail -n ${Tail}`
-else
-    SUBJECTS=`sed -n -E "s/sub-(\S*)\>.*/\1/gp" ${BIDS_DIR}/participants.tsv | head -n ${bigger_bit} | tail -n ${SUB_SIZE}`
-fi
-
-## set singularity environment variables that will point to the freesurfer license and the templateflow bits
-# Make sure FS_LICENSE is defined in the container.
-export SINGULARITYENV_FS_LICENSE=/home/qsiprep/.freesurfer.txt
-
-singularity run --cleanenv \
-    -B ${BASEDIR}/templates:/home/qsiprep --home /home/qsiprep \
-    -B ${BIDS_DIR}:/bids \
-    -B ${OUTPUT_DIR}:/derived \
-    -B ${WORK_DIR}:/work \
-    ${SING_CONTAINER} \
-    /bids /derived participant \
-    --participant_label ${SUBJECTS} \
-    -w /work \
-    --skip-bids-validation \
-    --omp-nthreads 8 \
-    --nthreads 40 \
-    --mem-mb 15000 \
-    --denoise-method dwidenoise \
-    --unringing-method mrdegibbs \
-    --separate_all_dwis \
-    --hmc_model eddy \
-    --output-resolution 2.0 \
-    --use-syn-sdc \
-    --force-syn
-
-exitcode=$?
-
-# Output results to a table
-for subject in $SUBJECTS; do
-    if [ $exitcode -eq 0 ]; then
-        echo "sub-$subject   ${SLURM_ARRAY_TASK_ID}    0" \
-            >> ${LOGS_DIR}/${SLURM_JOB_NAME}.${SLURM_ARRAY_JOB_ID}.tsv
-    else
-        echo "sub-$subject   ${SLURM_ARRAY_TASK_ID}    qsiprep failed" \
-            >> ${LOGS_DIR}/${SLURM_JOB_NAME}.${SLURM_ARRAY_JOB_ID}.tsv
-    fi
+for metric in FA MD RD AD; do
+${ENIGMA_DTI_BIDS}/run_group_enigma_concat.py \
+  ${OUT_DIR} ${metric} ${OUT_DIR}/group_enigmaDTI_${metric}.csv
+${ENIGMA_DTI_BIDS}/run_group_qc_index.py ${OUT_DIR} ${metric}skel
 done
+
+${ENIGMA_DTI_BIDS}/run_group_enigma_concat.py --output-nVox \a
+  ${OUT_DIR} FA ${OUT_DIR}/group_engimaDTI_nvoxels.csv
+
+python ${ENIGMA_DTI_BIDS}/run_group_dtifit_qc.py --debug /dtifit_dir
