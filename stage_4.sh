baf52b4e765b4184b9df5e5f7d9a91f51dfb850e
GhazalehManj <126309136+GhazalehManj@users.noreply.github.com>
2024-02-15 17:14:05 -0500

    Update stage_4.sh

--- stage_4.sh
+++ stage_4.sh
@@ -13,4 +13,13 @@ array_job_length=$(echo "$N_DTSERIES/${SUB_SIZE}" | bc)
 echo "number of array is: ${array_job_length}"
 
 ## submit the array job to the queue
-sbatch --array=0-${array_job_length} ./code/04_parcellate_scinet.sh
+sbatch --array=0-${array_job_length} ./code/04_parcellate_xcp_scinet.sh
+
+
+SUB_SIZE=10 # for func the sub size is moving to 1 participant because there are two runs and 8 tasks per run..
+N_SUBJECTS=$(( $( wc -l ./data/local/bids/participants.tsv | cut -f1 -d' ' ) - 1 ))
+array_job_length=$(echo "$N_SUBJECTS/${SUB_SIZE}" | bc)
+echo "number of array is: ${array_job_length}"
+
+## submit the array job to the queue
+sbatch --array=0-${array_job_length} ./code/04_parcellate_ciftify_scinet.sh
