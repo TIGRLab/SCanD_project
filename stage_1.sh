1fba75b1cfe25ddd56daf06a727c76e42fa654f3
GhazalehManj <126309136+GhazalehManj@users.noreply.github.com>
2024-03-06 13:31:37 -0500

    Update stage_1.sh

--- stage_1.sh
+++ stage_1.sh
@@ -16,7 +16,7 @@ array_job_length=$(echo "$N_SUBJECTS/${SUB_SIZE}" | bc)
 echo "number of array is: ${array_job_length}"
 
 ## submit the array job to the queue
-sbatch --array=0-${array_job_length} ./code/01_mriqc.sh
+sbatch --array=0-${array_job_length} ./code/01_mriqc_scinet.sh
 
 
 ##fmriprep_anat
