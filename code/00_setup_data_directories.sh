603e2215846cdc945c4b21941e88fb2b4dcd2c50
GhazalehManj <126309136+GhazalehManj@users.noreply.github.com>
2024-03-06 14:14:37 -0500

    Update 00_setup_data_directories.sh

--- code/00_setup_data_directories.sh
+++ code/00_setup_data_directories.sh
@@ -21,7 +21,7 @@ ln -s ${CONTAINER_DIR}/mriqc-22.0.6.simg containers/mriqc-22.0.6.simg
 ln -s ${CONTAINER_DIR}/qsiprep_0.16.0RC3.simg containers/qsiprep_0.16.0RC3.simg 
 ln -s ${CONTAINER_DIR}/xcp_d-0.6.0.simg containers/xcp_d-0.6.0.simg
 ln -s ${CONTAINER_DIR}/fmriprep_ciftity-v1.3.2-2.3.3.simg containers/fmriprep_ciftity-v1.3.2-2.3.3.simg 
-ln -s ${CONTAINER_DIR}/tbss2.simg containers/tbss_2023-10-10.simg
+ln -s ${CONTAINER_DIR}/tbss_2023-10-10.simg containers/tbss_2023-10-10.simg
 
 
 ## copy in Erin's freesurfer licence
