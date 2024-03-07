ec4d0873169bc0c90505b12e25f3a0fb83818307
GhazalehManj <126309136+GhazalehManj@users.noreply.github.com>
2024-03-07 09:18:11 -0500

    Update stage_3.sh

--- stage_3.sh
+++ stage_3.sh
@@ -1,4 +1,4 @@
-## stage 3 (ciftify_anat, xcp-d, enigma extract, enigma-dti):
+## stage 3 (ciftify_anat, xcp-d, enigma extract, enigma-dti, tractography):
 
 ## note step one is to make sure you are on one of the login nodes
 ssh niagara.scinet.utoronto.ca
@@ -26,6 +26,16 @@ echo "number of array is: ${array_job_length}"
 
 ## submit the array job to the queue
 sbatch --array=0-${array_job_length} ./code/03_xcp_scinet.sh
+
+##tractography
+## figuring out appropriate array-job size
+SUB_SIZE=2 # for func the sub size is moving to 1 participant because there are two runs and 8 tasks per run..
+N_SUBJECTS=$(( $( wc -l ./data/local/bids/participants.tsv | cut -f1 -d' ' ) - 1 ))
+array_job_length=$(echo "$N_SUBJECTS/${SUB_SIZE}" | bc)
+echo "number of array is: ${array_job_length}"
+
+## submit the array job to the queue
+sbatch --array=0-${array_job_length} ./code/02_tractography_scinet.sh
 
 ##enigma_extract
 source ./code/03_ENIGMA_ExtractCortical.sh
