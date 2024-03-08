77d9650c959fa04da5990e230ba5e2a22af05361
GhazalehManj <126309136+GhazalehManj@users.noreply.github.com>
2024-03-08 12:17:36 -0500

    Update 03_tractography_scinet.sh

--- code/03_tractography_scinet.sh
+++ code/03_tractography_scinet.sh
@@ -6,7 +6,7 @@
 #SBATCH --time=24:00:00
 
 
-SUB_SIZE=2 ## number of subjects to run is 1 because there are multiple tasks/run that will run in parallel 
+SUB_SIZE=1 ## number of subjects to run is 1 because there are multiple tasks/run that will run in parallel 
 CORES=40
 export THREADS_PER_COMMAND=2
 
