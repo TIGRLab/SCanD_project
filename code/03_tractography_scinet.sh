988b5e5e6c49ccbd8151b00a3c96b34ba8e33bc8
GhazalehManj <126309136+GhazalehManj@users.noreply.github.com>
2024-03-09 11:18:50 -0500

    Update 03_tractography_scinet.sh

--- code/03_tractography_scinet.sh
+++ code/03_tractography_scinet.sh
@@ -88,7 +88,7 @@ for subject in $SUBJECTS; do
         echo "sub-$subject   ${SLURM_ARRAY_TASK_ID}    0" \
             >> ${LOGS_DIR}/${SLURM_JOB_NAME}.${SLURM_ARRAY_JOB_ID}.tsv
     else
-        echo "sub-$subject   ${SLURM_ARRAY_TASK_ID}    qsiprep failed" \
+        echo "sub-$subject   ${SLURM_ARRAY_TASK_ID}    tractography failed" \
             >> ${LOGS_DIR}/${SLURM_JOB_NAME}.${SLURM_ARRAY_JOB_ID}.tsv
     fi
 done
