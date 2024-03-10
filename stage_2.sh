f165ecd7d1f50b2ec2cdaa72d3a0b804cc2a417f
GhazalehManj <126309136+GhazalehManj@users.noreply.github.com>
2024-03-09 22:44:10 -0500

    Update stage_2.sh

--- stage_2.sh
+++ stage_2.sh
@@ -17,7 +17,7 @@ echo "number of array is: ${array_job_length}"
 ## submit the array job to the queue
 sbatch --array=0-${array_job_length} ./code/02_fmriprep_func_scinet.sh
 
-##qsirecon
+##qsirecon step1
 ## figuring out appropriate array-job size
 SUB_SIZE=1 # for func the sub size is moving to 1 participant because there are two runs and 8 tasks per run..
 N_SUBJECTS=$(( $( wc -l ./data/local/bids/participants.tsv | cut -f1 -d' ' ) - 1 ))
