536a08b2944c5810622442071c22dfcdff39fb5d
GhazalehManj <126309136+GhazalehManj@users.noreply.github.com>
2024-03-09 22:42:29 -0500

    Update stage_2.sh

--- stage_2.sh
+++ stage_2.sh
@@ -1,4 +1,4 @@
-## stage 2 (fmriprep_func, qsirecon):
+## stage 2 (fmriprep_func, qsirecon_step1):
 
 ## note step one is to make sure you are on one of the login nodes
 ssh nia-login07
@@ -25,4 +25,4 @@ array_job_length=$(echo "$N_SUBJECTS/${SUB_SIZE}" | bc)
 echo "number of array is: ${array_job_length}"
 
 ## submit the array job to the queue
-sbatch --array=0-${array_job_length} ./code/02_qsirecon_scinet.sh
+sbatch --array=0-${array_job_length} ./code/02_qsirecon_step1_scinet.sh
