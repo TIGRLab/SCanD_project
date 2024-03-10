025a673aedaa670eb66b03325f97671d540e6daf
GhazalehManj <126309136+GhazalehManj@users.noreply.github.com>
2024-03-10 00:02:26 -0500

    Update stage_5.sh

--- stage_5.sh
+++ stage_5.sh
@@ -1,8 +1,3 @@
 ## stage 5 (extract and hare files):
-ssh niagara.scinet.utoronto.ca
-
-## go to the repo and pull new changes
-cd ${SCRATCH}/SCanD_project_GMANJ
-git pull
 
 source ./code/05_extract_to_share.sh
