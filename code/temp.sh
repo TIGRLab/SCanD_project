d11429b2b9fc780769250a6b6251228677401aac
GhazalehManj <126309136+GhazalehManj@users.noreply.github.com>
2024-03-20 15:57:35 -0400

    Update temp.sh

--- code/temp.sh
+++ code/temp.sh
@@ -11,10 +11,11 @@ BASEDIR=${SLURM_SUBMIT_DIR}
 
 ## this script requires gnu-parallel
 module load gnu-parallel/20191122
+SUB_SIZE=10 
 
 ## note the dlabel file path must be a relative to the output folder
 export parcellation_dir=${BASEDIR}/templates/parcellations
-export atlases="atlas-Glasser"
+export atlases=( $(ls ${parcellation_dir}/tpl-fsLR_res-91k_atlas-*.dlabel.nii | xargs -n 1 basename | sed -E 's/^.*atlas-(.*?)_.*$/atlas-\1/' | sort -u) )
 
 ## set up a trap that will clear the ramdisk if it is not cleared
 function cleanup_ramdisk {
