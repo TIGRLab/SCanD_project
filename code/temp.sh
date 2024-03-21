53533ca18e9eec76f12d5a37609f65f628ca69b6
GhazalehManj <126309136+GhazalehManj@users.noreply.github.com>
2024-03-20 21:53:53 -0400

    Update temp.sh

--- code/temp.sh
+++ code/temp.sh
@@ -15,9 +15,7 @@ SUB_SIZE=10
 
 ## note the dlabel file path must be a relative to the output folder
 export parcellation_dir=${BASEDIR}/templates/parcellations
-#export atlases=( $(ls ${parcellation_dir}/tpl-fsLR_res-91k_atlas-*.dlabel.nii | xargs -n 1 basename | sed -E 's/^.*atlas-(.*?)_.*$/atlas-\1/' | sort -u) )
-#export atlases="atlas-Gordon"
-export atlases="atlas-Glasser atlas-Gordon"
+export atlases="atlas-4S1056Parcels atlas-4S156Parcels atlas-4S256Parcels atlas-4S356Parcels atlas-4S456Parcel atlas-4S556Parcels atlas-4S656Parcels atlas-4S756Parcels atlas-4S856Parcels  atlas-4S956Parcels atlas-Glasser atlas-Gordon atlas-HCP atlas-Tian"
 
 
 ## set up a trap that will clear the ramdisk if it is not cleared
