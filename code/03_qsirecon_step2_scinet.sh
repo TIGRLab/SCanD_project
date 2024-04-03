8645471f52a5ef277fc8cda9c8c110f4e77988bf
GhazalehManj <126309136+GhazalehManj@users.noreply.github.com>
2024-04-03 15:29:04 -0400

    Update 03_qsirecon_step2_scinet.sh

--- code/03_qsirecon_step2_scinet.sh
+++ code/03_qsirecon_step2_scinet.sh
@@ -34,7 +34,7 @@ export BIDS_DIR=${BASEDIR}/data/local/bids
 ## these folders envs need to be set up for this script to run properly 
 ## see notebooks/00_setting_up_envs.md for the set up instructions
 export QSIPREP_HOME=${BASEDIR}/templates
-export SING_CONTAINER=${BASEDIR}/containers/qsiprep_0.16.0RC3.simg
+export SING_CONTAINER=${BASEDIR}/containers/qsiprep_0.19.0.sif
 
 ## setting up the output folders
 export OUTPUT_DIR=${BASEDIR}/data/local  # use if version of fmriprep >=20.2
