338d62184e1184f5f194d32639de05ec6c9170db
GhazalehManj <126309136+GhazalehManj@users.noreply.github.com>
2024-04-03 12:42:28 -0400

    Update stage_4.sh

--- stage_4.sh
+++ stage_4.sh
@@ -1,13 +1,4 @@
-## stage 4 (parcellations):
-
-
-SUB_SIZE=10 # for func the sub size is moving to 1 participant because there are two runs and 8 tasks per run..
-N_DTSERIES=$(ls -1d ./data/local/xcp_d/sub*/ses*/func/*dtseries* | wc -l)
-array_job_length=$(echo "$N_DTSERIES/${SUB_SIZE}" | bc)
-echo "number of array is: ${array_job_length}"
-
-## submit the array job to the queue
-sbatch --array=0-${array_job_length} ./code/04_parcellate_xcp_scinet.sh
+## stage 4 (parcellation-ciftify):
 
 
 SUB_SIZE=10 # for func the sub size is moving to 1 participant because there are two runs and 8 tasks per run..
