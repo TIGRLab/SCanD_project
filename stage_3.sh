64fd648f7c785b0e1a9aa0a52ec42a1c990b63b2
GhazalehManj <126309136+GhazalehManj@users.noreply.github.com>
2024-04-11 23:46:54 -0400

    Update stage_3.sh

--- stage_3.sh
+++ stage_3.sh
@@ -1,4 +1,4 @@
-## stage 3 (ciftify_anat, xcp-d, enigma extract, enigma-dti, qsirecon_step2):
+## stage 3 (ciftify_anat, xcp-d, qsirecon_step2, tractography, enigma extract, enigma-dti):
 
 
 ##ciftify_anat
@@ -32,8 +32,20 @@ echo "number of array is: ${array_job_length}"
 ## submit the array job to the queue
 sbatch --array=0-${array_job_length} ./code/03_qsirecon_step2_scinet.sh
 
+
+## tractography
+## figuring out appropriate array-job size
+SUB_SIZE=1 # for func the sub size is moving to 1 participant because there are two runs and 8 tasks per run..
+N_SUBJECTS=$(( $( wc -l ./data/local/bids/participants.tsv | cut -f1 -d' ' ) - 1 ))
+array_job_length=$(echo "$N_SUBJECTS/${SUB_SIZE}" | bc)
+echo "number of array is: ${array_job_length}"
+
+## submit the array job to the queue
+sbatch --array=0-${array_job_length} ./code/03_tractography_scinet.sh
+
 ##enigma_extract
 source ./code/03_ENIGMA_ExtractCortical.sh
 
 ##enigma-dti
 sbatch  ./code/03_enigma_dti_scinet.sh
+
