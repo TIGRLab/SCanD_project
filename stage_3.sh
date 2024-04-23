7cc0aaa1753cd9185ad134280eb25b7486bae62c
GhazalehManj <126309136+GhazalehManj@users.noreply.github.com>
2024-04-23 13:34:55 -0400

    Update stage_3.sh

--- stage_3.sh
+++ stage_3.sh
@@ -12,20 +12,8 @@ echo "number of array is: ${array_job_length}"
 sbatch --array=0-${array_job_length} ./code/03_ciftify_anat_scinet.sh
 
 ##xcp_d
-module load NiaEnv/2019b python/3.11.5
-
-# Create a directory for virtual environments if it doesn't exist
-mkdir ~/.virtualenvs
-cd ~/.virtualenvs
-virtualenv --system-site-packages ~/.virtualenvs/myenv
-
-# Activate the virtual environment
-source ~/.virtualenvs/myenv/bin/activate 
-
-python3 -m pip install -U templateflow
-
-# Run a Python script to import specified templates using the 'templateflow' package
-python -c "from templateflow.api import get; get(['fsLR', 'Fischer344','MNI152Lin'])"
+mkdir $HOME/.cache/templateflow/
+rsync -av  templateflow/* $HOME/.cache/templateflow/
 
 ## figuring out appropriate array-job size
 SUB_SIZE=1 # for func the sub size is moving to 1 participant because there are two runs and 8 tasks per run..
