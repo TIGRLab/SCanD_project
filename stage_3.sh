a70f6cee49c66655bbf28152f8031e4ed61f3863
GhazalehManj <126309136+GhazalehManj@users.noreply.github.com>
2024-04-23 14:34:09 -0400

    Update stage_3.sh

--- stage_3.sh
+++ stage_3.sh
@@ -12,9 +12,6 @@ echo "number of array is: ${array_job_length}"
 sbatch --array=0-${array_job_length} ./code/03_ciftify_anat_scinet.sh
 
 ##xcp_d
-mkdir $HOME/.cache/templateflow/
-rsync -av  templateflow/* $HOME/.cache/templateflow/
-
 ## figuring out appropriate array-job size
 SUB_SIZE=1 # for func the sub size is moving to 1 participant because there are two runs and 8 tasks per run..
 N_SUBJECTS=$(( $( wc -l ./data/local/bids/participants.tsv | cut -f1 -d' ' ) - 1 ))
