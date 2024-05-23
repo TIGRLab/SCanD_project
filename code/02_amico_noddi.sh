560006ac50730ee75d7936d95de2975d721b115c
GhazalehManj <126309136+GhazalehManj@users.noreply.github.com>
2024-05-23 12:43:47 -0400

    Update 02_amico_noddi.sh

--- code/02_amico_noddi.sh
+++ code/02_amico_noddi.sh
@@ -55,7 +55,7 @@ fi
 
 export SINGULARITYENV_FS_LICENSE=${BASEDIR}/templates/.freesurfer.txt
 
-xvfb-run singularity run \
+singularity run --cleanenv \
   -H ${TMP_DIR} \
   -B ${BIDS_DIR}:/bids \
   -B ${QSIPREP_DIR}:/qsiprep \
