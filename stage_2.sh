30b8c91d127dd75bb62a204170d45876a3540457
GhazalehManj <126309136+GhazalehManj@users.noreply.github.com>
2024-05-23 11:12:06 -0400

    Update stage_2.sh

--- stage_2.sh
+++ stage_2.sh
@@ -24,7 +24,7 @@ sbatch --array=0-${array_job_length} ./code/02_qsirecon_step1_scinet.sh
 
 ##amico_noddi
 ## figuring out appropriate array-job size
-SUB_SIZE=2 # for func the sub size is moving to 1 participant because there are two runs and 8 tasks per run..
+SUB_SIZE=1 # for func the sub size is moving to 1 participant because there are two runs and 8 tasks per run..
 N_SUBJECTS=$(( $( wc -l ./data/local/bids/participants.tsv | cut -f1 -d' ' ) - 1 ))
 array_job_length=$(echo "$N_SUBJECTS/${SUB_SIZE}" | bc)
 echo "number of array is: ${array_job_length}"
