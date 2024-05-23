de2c4d8247d15a89fe7509afd0c84398bcb39560
GhazalehManj <126309136+GhazalehManj@users.noreply.github.com>
2024-05-23 09:25:58 -0400

    Update stage_2.sh

--- stage_2.sh
+++ stage_2.sh
@@ -1,4 +1,4 @@
-## stage 2 (fmriprep_func, qsirecon_step1):
+## stage 2 (fmriprep_func, qsirecon_step1, amico_noddi):
 
 
 ##fmriprep_func
@@ -20,3 +20,14 @@ echo "number of array is: ${array_job_length}"
 
 ## submit the array job to the queue
 sbatch --array=0-${array_job_length} ./code/02_qsirecon_step1_scinet.sh
+
+
+##amico_noddi
+## figuring out appropriate array-job size
+SUB_SIZE=2 # for func the sub size is moving to 1 participant because there are two runs and 8 tasks per run..
+N_SUBJECTS=$(( $( wc -l ./data/local/bids/participants.tsv | cut -f1 -d' ' ) - 1 ))
+array_job_length=$(echo "$N_SUBJECTS/${SUB_SIZE}" | bc)
+echo "number of array is: ${array_job_length}"
+
+## submit the array job to the queue
+sbatch --array=0-${array_job_length} ./code/02_amico_noddi.sh
