df4b5ace876fef7bba91881a81ffa57ecd6b95bc
GhazalehManj <126309136+GhazalehManj@users.noreply.github.com>
2024-05-27 14:40:39 -0400

    Update 05_extract_to_share.sh

--- code/05_extract_to_share.sh
+++ code/05_extract_to_share.sh
@@ -3,13 +3,17 @@
 
 ## copying the fmriprep QA files and figures plus logs and metadata to 
 
-echo "copying over the fmriprep metadata and qc images"
-
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 PROJECT_DIR=$(dirname "${SCRIPT_DIR}")
 
-FMRIPREP_SHARE_DIR=${PROJECT_DIR}/data/share/fmriprep
-FMRIPREP_LOCAL_DIR=${PROJECT_DIR}/data/local/fmriprep
+FMRIPREP_SHARE_DIR=${PROJECT_DIR}/data/share/fmriprep/23.2.0
+FMRIPREP_LOCAL_DIR=${PROJECT_DIR}/data/local/derivatives/fmriprep/23.2.0
+
+if [ -d "$FMRIPREP_LOCAL_DIR" ]; 
+then
+  
+  echo "Copying FMRIPREP metatdata and QC images"
+
 
 mkdir -p ${FMRIPREP_SHARE_DIR}
 
@@ -24,12 +28,22 @@ for subject in ${subjects}; do
  rsync -a ${FMRIPREP_LOCAL_DIR}/${subject}/figures ${FMRIPREP_SHARE_DIR}/${subject}/
 done
 
+else
 
-echo "copying over the qsiprep metadata and qc images"
+    echo "FMRIPREP outputs not found."
+
+fi
+
+
+
 ## copy over the qsiprep json files (for https://www.nipreps.org/dmriprep-viewer/#/)
 QSIPREP_SHARE_DIR=${PROJECT_DIR}/data/share/qsiprep
 QSIPREP_LOCAL_DIR=${PROJECT_DIR}/data/local/qsiprep
 
+if [ -d "$QSIPREP_LOCAL_DIR" ]; 
+then
+
+echo "copying over the qsiprep metadata and qc images"
 rsync -a --include "*/" --include="*.json" --exclude="*" ${QSIPREP_LOCAL_DIR} ${QSIPREP_SHARE_DIR}
 
 ## copy over the qsiprep html files
@@ -40,20 +54,41 @@ for subject in ${subjects}; do
  rsync -a ${QSIPREP_LOCAL_DIR}/${subject}/figures ${QSIPREP_SHARE_DIR}/${subject}/
 done
 
+else
+
+    echo "QSIPREP (DWI) outputs not found"
+
+fi
+
 
 ## run the mriqc group step and copy over all outputs
+MRIQC_SHARE_DIR=${PROJECT_DIR}/data/share/mriqc/22.0.6
+MRIQC_LOCAL_DIR=${PROJECT_DIR}/data/local/derivatives/mriqc/22.0.6
+
+if [ -d "$MRIQC_LOCAL_DIR" ]; 
+then
+
 echo "running mriqc group and copying files"
 singularity run --cleanenv \
     -B ${PROJECT_DIR}/templates:/home/mriqc --home /home/mriqc \
     -B ${PROJECT_DIR}/data/local/bids:/bids \
-    -B ${PROJECT_DIR}/data/local/mriqc:/derived \
+    -B ${$MRIQC_LOCAL_DIR}:/derived \
     ${PROJECT_DIR}/containers/mriqc-22.0.6.simg \
     /bids /derived group 
 
-mkdir ${PROJECT_DIR}/data/share/mriqc
-rsync -a ${PROJECT_DIR}/data/local/mriqc/dataset_description.json ${PROJECT_DIR}/data/share/mriqc/
-rsync -a ${PROJECT_DIR}/data/local/mriqc/group*.tsv ${PROJECT_DIR}/data/share/mriqc/
+mkdir -p ${MRIQC_SHARE_DIR}
+rsync -a ${$MRIQC_LOCAL_DIR}/dataset_description.json ${MRIQC_SHARE_DIR}/
+rsync -a ${$MRIQC_LOCAL_DIR}/group*.tsv ${MRIQC_SHARE_DIR}/
 
+else
+
+    echo "No MRIQC outputs found."
+
+fi
+
+
+if [ -d "${PROJECT_DIR}/data/local/xcp_d" ]; 
+then
 
 echo "copying over the xcp_d folder"
 
@@ -64,6 +99,13 @@ rm -rf ${PROJECT_DIR}/data/share/xcp_d
 ## copy over the xcp  folder (all data)
 rsync -a ${PROJECT_DIR}/data/local/xcp_d  ${PROJECT_DIR}/data/share
 
+else
+    echo "No XCP_D outputs found."
+fi
+
+
+if [ -d "${PROJECT_DIR}/data/local/ciftify" ]; 
+then
 
 ## also run ciftify group step
 echo "copying over the ciftify qc images"
@@ -71,28 +113,47 @@ echo "copying over the ciftify qc images"
 ## copy over the ciftify QC outputs
 rsync -a ${PROJECT_DIR}/data/local/ciftify/qc_recon_all  ${PROJECT_DIR}/data/share/ciftify/
 
+else
+
+    echo "No ciftify outputs found."
+
+fi
+
 
 
 ## copy over the Enigma_extract outputs
+if [ -d "${PROJECT_DIR}/data/local/ENIGMA_extract" ]; 
+then
 echo "copying over the ENIGMA extracted cortical and subcortical files"
 rsync -a ${PROJECT_DIR}/data/local/ENIGMA_extract ${PROJECT_DIR}/data/share/
+fi
 
 ## copy over the parcellated_ciftify files
+if [ -d "${PROJECT_DIR}/data/local/parcellated_ciftify" ]; 
+then
 echo "copying over the parcellated_ciftify files"
 rsync -a ${PROJECT_DIR}/data/local/parcellated_ciftify ${PROJECT_DIR}/data/share/
-
+fi
 
 ## copy over the enigmaDTI files
+if [ -d "${PROJECT_DIR}/data/local/enigmaDTI" ]; 
+then
 echo "copying over the enigmaDTI files"
 mkdir ${PROJECT_DIR}/data/share/enigmaDTI
 rsync -a ${PROJECT_DIR}/data/local/enigmaDTI/group*  ${PROJECT_DIR}/data/share/enigmaDTI
 rsync -a ${PROJECT_DIR}/data/local/enigmaDTI/*.html  ${PROJECT_DIR}/data/share/enigmaDTI
 
 rsync -a --include "*/" --include "*.png" --exclude "*" ${PROJECT_DIR}/data/local/enigmaDTI/ ${PROJECT_DIR}/data/share/enigmaDTI
+fi
 
-echo "copying over the amico noddi metadata and qc images"
+
+
 AMICO_SHARE_DIR=${PROJECT_DIR}/data/share/amico
 AMICO_LOCAL_DIR=${PROJECT_DIR}/data/local/amico_noddi
+
+if [ -d "${AMICO_LOCAL_DIR}" ]; 
+then
+echo "copying over the amico noddi metadata and qc images"
 
 ## copy over the amico noddi html files
 subjects=`cd ${AMICO_LOCAL_DIR}/qsirecon; ls -1d sub-* | grep -v html`
@@ -102,3 +163,9 @@ for subject in ${subjects}; do
  mkdir -p ${AMICO_SHARE_DIR}/${subject}/figures
  rsync -a ${AMICO_LOCAL_DIR}/qsirecon/${subject}/figures ${AMICO_SHARE_DIR}/${subject}/
 done
+
+else
+
+echo "No NODDI outputs found."
+
+fi
