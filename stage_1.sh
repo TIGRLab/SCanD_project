8a0c06f11d7cc201c110d59e3d5818a6a8fe3bad
Erin W Dickie <erin.w.dickie@gmail.com>
2024-05-27 21:18:58 -0400

    adding freesurfer long script

--- stage_1.sh
+++ stage_1.sh
@@ -13,9 +13,9 @@ sbatch --array=0-${array_job_length} ./code/01_mriqc_scinet.sh
 
 
 ##fmriprep_fit
-## figuring out appropriate array-job size
-mkdir -p ./data/local/derivatives/fmriprep/23.2.3/sourcedata/freesurfer
-rsync -av /project/a/arisvoin/edickie/fsaverage ./data/local/derivatives/fmriprep/23.2.3/sourcedata/freesurfer/
+## note this set-up avoids fmriprep failing due to weird conflicts while writing to fsaverage foler
+# mkdir -p ./data/local/derivatives/fmriprep/23.2.3/sourcedata/freesurfer
+# rsync -av /project/a/arisvoin/edickie/fsaverage ./data/local/derivatives/fmriprep/23.2.3/sourcedata/freesurfer/
 SUB_SIZE=1
 N_SUBJECTS=$(( $( wc -l ./data/local/bids/participants.tsv | cut -f1 -d' ' ) - 1 ))
 array_job_length=$(echo "$N_SUBJECTS/${SUB_SIZE}" | bc)
@@ -23,6 +23,15 @@ echo "number of array is: ${array_job_length}"
 
 ## submit the array job to the queue
 sbatch --array=0-${array_job_length} code/01_fmriprep_fit_scinet.sh
+
+## submitting freesurfer longitudinal
+SUB_SIZE=1
+N_SUBJECTS=$(( $( wc -l ./data/local/bids/participants.tsv | cut -f1 -d' ' ) - 1 ))
+array_job_length=$(echo "$N_SUBJECTS/${SUB_SIZE}" | bc)
+echo "number of array is: ${array_job_length}"
+
+## submit the array job to the queue
+sbatch --array=0-${array_job_length} code/01_freesurfer_long_scinet.sh
 
 
 #qsiprep
