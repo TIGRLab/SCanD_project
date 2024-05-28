a61b3dabb02c6b1a01c146a3606a3b8815133b09
GhazalehManj <126309136+GhazalehManj@users.noreply.github.com>
2024-05-27 21:08:56 -0400

    Update stage_1.sh

--- stage_1.sh
+++ stage_1.sh
@@ -1,34 +1,62 @@
-## stage 1 (mriqc, fmriprep_anat, qsiprep):
+#!/bin/bash
 
+# Ask the user whether to run only functional codes
+read -p "Do you want to only run functional codes? (yes/no): " RUN_FUNCTIONAL_ONLY
 
-##mriqc
-## calculate the length of the array-job given
-SUB_SIZE=4
-N_SUBJECTS=$(( $( wc -l ./data/local/bids/participants.tsv | cut -f1 -d' ' ) - 1 ))
-array_job_length=$(echo "$N_SUBJECTS/${SUB_SIZE}" | bc)
-echo "number of array is: ${array_job_length}"
+if [ "$RUN_FUNCTIONAL_ONLY" = "yes" ] || [ "$RUN_FUNCTIONAL_ONLY" = "y" ]; then
+    # Run only mriqc and fmriprep_anat
+    echo "Running only functional codes: mriqc and fmriprep_anat"
 
-## submit the array job to the queue
-sbatch --array=0-${array_job_length} ./code/01_mriqc_scinet.sh
+    ## mriqc
+    ## calculate the length of the array-job given
+    SUB_SIZE=4
+    N_SUBJECTS=$(( $( wc -l ./data/local/bids/participants.tsv | cut -f1 -d' ' ) - 1 ))
+    array_job_length=$(echo "$N_SUBJECTS/${SUB_SIZE}" | bc)
+    echo "number of array is: ${array_job_length}"
 
+    ## submit the array job to the queue
+    sbatch --array=0-${array_job_length} ./code/01_mriqc_scinet.sh
 
-##fmriprep_anat
-## figuring out appropriate array-job size
-SUB_SIZE=1
-N_SUBJECTS=$(( $( wc -l ./data/local/bids/participants.tsv | cut -f1 -d' ' ) - 1 ))
-array_job_length=$(echo "$N_SUBJECTS/${SUB_SIZE}" | bc)
-echo "number of array is: ${array_job_length}"
+    ## fmriprep_anat
+    ## figuring out appropriate array-job size
+    SUB_SIZE=1
+    N_SUBJECTS=$(( $( wc -l ./data/local/bids/participants.tsv | cut -f1 -d' ' ) - 1 ))
+    array_job_length=$(echo "$N_SUBJECTS/${SUB_SIZE}" | bc)
+    echo "number of array is: ${array_job_length}"
 
-## submit the array job to the queue
-sbatch --array=0-${array_job_length} code/01_fmriprep_anat_scinet.sh
+    ## submit the array job to the queue
+    sbatch --array=0-${array_job_length} code/01_fmriprep_anat_scinet.sh
+else
+    # Run all three codes: mriqc, fmriprep_anat, and qsiprep
+    echo "Running all codes: mriqc, fmriprep_anat, and qsiprep"
 
+    ## mriqc
+    ## calculate the length of the array-job given
+    SUB_SIZE=4
+    N_SUBJECTS=$(( $( wc -l ./data/local/bids/participants.tsv | cut -f1 -d' ' ) - 1 ))
+    array_job_length=$(echo "$N_SUBJECTS/${SUB_SIZE}" | bc)
+    echo "number of array is: ${array_job_length}"
 
-#qsiprep
-## figuring out appropriate array-job size
-SUB_SIZE=2 # for func the sub size is moving to 1 participant because there are two runs and 8 tasks per run..
-N_SUBJECTS=$(( $( wc -l ./data/local/bids/participants.tsv | cut -f1 -d' ' ) - 1 ))
-array_job_length=$(echo "$N_SUBJECTS/${SUB_SIZE}" | bc)
-echo "number of array is: ${array_job_length}"
+    ## submit the array job to the queue
+    sbatch --array=0-${array_job_length} ./code/01_mriqc_scinet.sh
 
-## submit the array job to the queue
-sbatch --array=0-${array_job_length} ./code/01_qsiprep_scinet.sh
+    ## fmriprep_anat
+    ## figuring out appropriate array-job size
+    SUB_SIZE=1
+    N_SUBJECTS=$(( $( wc -l ./data/local/bids/participants.tsv | cut -f1 -d' ' ) - 1 ))
+    array_job_length=$(echo "$N_SUBJECTS/${SUB_SIZE}" | bc)
+    echo "number of array is: ${array_job_length}"
+
+    ## submit the array job to the queue
+    sbatch --array=0-${array_job_length} code/01_fmriprep_anat_scinet.sh
+
+    ## qsiprep
+    ## figuring out appropriate array-job size
+    SUB_SIZE=2 # for func the sub size is moving to 1 participant because there are two runs and 8 tasks per run..
+    N_SUBJECTS=$(( $( wc -l ./data/local/bids/participants.tsv | cut -f1 -d' ' ) - 1 ))
+    array_job_length=$(echo "$N_SUBJECTS/${SUB_SIZE}" | bc)
+    echo "number of array is: ${array_job_length}"
+
+    ## submit the array job to the queue
+    sbatch --array=0-${array_job_length} ./code/01_qsiprep_scinet.sh
+fi
