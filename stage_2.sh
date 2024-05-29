4fb337f241ee59e03037084f7699c0942bfa1d0e
GhazalehManj <126309136+GhazalehManj@users.noreply.github.com>
2024-05-29 13:59:28 -0400

    Update stage_2.sh

--- stage_2.sh
+++ stage_2.sh
@@ -19,9 +19,13 @@ if [ "$RUN_FMRIPREP_FUNC_ONLY" = "yes" ] || [ "$RUN_FMRIPREP_FUNC_ONLY" = "y" ];
     sbatch --array=0-${array_job_length} ./code/02_fmriprep_func_scinet.sh
 
 else
-
     # Ask the user whether the data is single-shell or multi-shell
     read -p "Is the data single-shell or multi-shell? (single/multi): " DATA_SHELL_TYPE
+
+    if [ "$DATA_SHELL_TYPE" != "single" ] && [ "$DATA_SHELL_TYPE" != "multi" ]; then
+        echo "Invalid input. Please specify either 'single' or 'multi' for the data shell type."
+        exit 1
+    fi
 
     # Stage 2: fmriprep_func
     # Figuring out appropriate array-job size
@@ -44,7 +48,7 @@ else
     sbatch --array=0-${array_job_length} ./code/02_qsirecon_step1_scinet.sh
 
     if [ "$DATA_SHELL_TYPE" = "multi" ]; then
-       echo "Running all codes: fmriprep_func, qsirecon_step1, and possibly amico_noddi"
+        echo "Running all codes: fmriprep_func, qsirecon_step1, and amico_noddi"
        
         # Stage 2: amico_noddi (only for multi-shell data)
         # Figuring out appropriate array-job size
