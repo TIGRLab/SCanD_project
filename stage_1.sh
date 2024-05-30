ee7bd86323de9a39f178f92c66d1e6eec46cc4bd
GhazalehManj <126309136+GhazalehManj@users.noreply.github.com>
2024-05-30 16:01:05 -0400

    Update stage_1.sh

--- stage_1.sh
+++ stage_1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#stage1 (mriqc, fmriprep_anat, qsiprep):
+#stage1 (mriqc, fmriprep_fit, freesurfer, qsiprep):
 # Ask the user whether to run only functional codes
 read -p "Do you want to only run functional pipelines? (yes/no): " RUN_FUNCTIONAL_ONLY
 
@@ -18,15 +18,26 @@ if [ "$RUN_FUNCTIONAL_ONLY" = "yes" ] || [ "$RUN_FUNCTIONAL_ONLY" = "y" ]; then
     ## submit the array job to the queue
     sbatch --array=0-${array_job_length} ./code/01_mriqc_scinet.sh
 
-    ## fmriprep_anat
+    ## fmriprep_fit
     ## figuring out appropriate array-job size
+
     SUB_SIZE=1
     N_SUBJECTS=$(( $( wc -l ./data/local/bids/participants.tsv | cut -f1 -d' ' ) - 1 ))
     array_job_length=$(echo "$N_SUBJECTS/${SUB_SIZE}" | bc)
     echo "number of array is: ${array_job_length}"
 
     ## submit the array job to the queue
-    sbatch --array=0-${array_job_length} code/01_fmriprep_anat_scinet.sh
+    sbatch --array=0-${array_job_length} code/01_fmriprep_fit_scinet.sh
+
+
+    ## submitting freesurfer longitudinal
+    SUB_SIZE=1
+    N_SUBJECTS=$(( $( wc -l ./data/local/bids/participants.tsv | cut -f1 -d' ' ) - 1 ))
+    array_job_length=$(echo "$N_SUBJECTS/${SUB_SIZE}" | bc)
+    echo "number of array is: ${array_job_length}"
+
+    ## submit the array job to the queue
+    sbatch --array=0-${array_job_length} code/01_freesurfer_long_scinet.sh
 else
     # Run all three codes: mriqc, fmriprep_anat, and qsiprep
     echo "Running all codes: mriqc, fmriprep_anat, and qsiprep"
@@ -41,7 +52,7 @@ else
     ## submit the array job to the queue
     sbatch --array=0-${array_job_length} ./code/01_mriqc_scinet.sh
 
-    ## fmriprep_anat
+    ## fmriprep_fit
     ## figuring out appropriate array-job size
     SUB_SIZE=1
     N_SUBJECTS=$(( $( wc -l ./data/local/bids/participants.tsv | cut -f1 -d' ' ) - 1 ))
@@ -49,7 +60,16 @@ else
     echo "number of array is: ${array_job_length}"
 
     ## submit the array job to the queue
-    sbatch --array=0-${array_job_length} code/01_fmriprep_anat_scinet.sh
+    sbatch --array=0-${array_job_length} code/01_fmriprep_fit_scinet.sh
+
+    ## submitting freesurfer longitudinal
+    SUB_SIZE=1
+    N_SUBJECTS=$(( $( wc -l ./data/local/bids/participants.tsv | cut -f1 -d' ' ) - 1 ))
+    array_job_length=$(echo "$N_SUBJECTS/${SUB_SIZE}" | bc)
+    echo "number of array is: ${array_job_length}"
+
+    ## submit the array job to the queue
+    sbatch --array=0-${array_job_length} code/01_freesurfer_long_scinet.sh
 
     ## qsiprep
     ## figuring out appropriate array-job size
