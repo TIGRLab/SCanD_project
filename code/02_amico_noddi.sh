30a8d3683ce0e118adf8dd175d7313a66177c036
GhazalehManj <126309136+GhazalehManj@users.noreply.github.com>
2024-05-30 10:53:09 -0400

    Update 02_amico_noddi.sh

--- code/02_amico_noddi.sh
+++ code/02_amico_noddi.sh
@@ -30,7 +30,7 @@ trap "cleanup_ramdisk" TERM
 
 # input is BIDS_DIR this is where the data downloaded from openneuro went
 export BIDS_DIR=${BASEDIR}/data/local/bids
-export QSIPREP_DIR=${BASEDIR}/data/local/derivatives/qsiprep/0.21.4
+export QSIPREP_DIR=${BASEDIR}/data/local/derivatives/qsiprep/0.21.4/qsiprep
 export SING_CONTAINER=${BASEDIR}/containers/qsiprep-0.21.4.sif
 export OUTPUT_DIR=${BASEDIR}/data/local/derivatives/qsiprep/0.21.4/amico_noddi
 export TMP_DIR=${BASEDIR}/data/local/derivatives/qsiprep/0.21.4/amico_noddi/tmp
