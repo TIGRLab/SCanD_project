37420f1b0e87491edb1d7dce0cc339c0ab14b1a1
GhazalehManj <126309136+GhazalehManj@users.noreply.github.com>
2024-05-30 15:45:57 -0400

    Update 02_fmriprep_func_scinet.sh

--- code/02_fmriprep_func_scinet.sh
+++ code/02_fmriprep_func_scinet.sh
@@ -40,8 +40,8 @@ export SING_CONTAINER=${BASEDIR}/containers/fmriprep-20.2.7.simg
 #export OUTPUT_DIR=${BASEDIR}/data/local/fmriprep  # use if version of fmriprep >=20.2
 export OUTPUT_DIR=${BASEDIR}/data/local/ # use if version of fmriprep <=21.0
 
-
-export WORK_DIR=${BBUFFER}/SCanD/fmriprep
+project_id=$(cat ${BASEDIR}/project_id)
+export WORK_DIR=${BBUFFER}/SCanD/${project_id}/fmriprep
 export LOGS_DIR=${BASEDIR}/logs
 mkdir -vp ${OUTPUT_DIR} ${WORK_DIR} # ${LOCAL_FREESURFER_DIR}
 
