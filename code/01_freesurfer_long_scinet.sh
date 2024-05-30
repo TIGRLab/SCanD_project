cc7680ec2f41c9e793878d60dd940031a629b380
GhazalehManj <126309136+GhazalehManj@users.noreply.github.com>
2024-05-30 15:59:22 -0400

    Create 01_freesurfer_long_scinet.sh

---
+++ code/01_freesurfer_long_scinet.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+#SBATCH --job-name=freesurfer
+#SBATCH --output=logs/%x_%j.out 
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=40
+#SBATCH --time=16:00:00
+
+
+SUB_SIZE=1 ## number of subjects to run
+
+
+####----### the next bit only works IF this script is submitted from the $BASEDIR/$OPENNEURO_DS folder...
+
+## set the second environment variable to get the base directory
+BASEDIR=${SLURM_SUBMIT_DIR}
+
+## set up a trap that will clear the ramdisk if it is not cleared
+function cleanup_ramdisk {
+    echo -n "Cleaning up ramdisk directory /$SLURM_TMPDIR/ on "
+    date
+    rm -rf /$SLURM_TMPDIR
+    echo -n "done at "
+    date
+}
+
+#trap the termination signal, and call the function 'trap_term' when
+# that happens, so results may be saved.
+trap "cleanup_ramdisk" TERM
+
+# input is BIDS_DIR this is where the data downloaded from openneuro went
+export BIDS_DIR=${BASEDIR}/data/local/bids
+
+## these folders envs need to be set up for this script to run properly 
+## see notebooks/00_setting_up_envs.md for the set up instructions
+export FMRIPREP_HOME=${BASEDIR}/templates
+export SING_CONTAINER=${BASEDIR}/containers/freesurfer-6.0.1.simg
+
+
+## setting up the output folders
+export OUTPUT_DIR=${BASEDIR}/data/local/freesurfer_long  # use if version of fmriprep >=20.2
+#export OUTPUT_DIR=${BASEDIR}/data/local/ # use if version of fmriprep <=21.0
+
+# export LOCAL_FREESURFER_DIR=${SCRATCH}/${STUDY}/data/derived/freesurfer-6.0.1
+export LOGS_DIR=${BASEDIR}/logs
+mkdir -vp ${OUTPUT_DIR} ${LOGS_DIR} # ${LOCAL_FREESURFER_DIR}
+
+## get the subject list from a combo of the array id, the participants.tsv and the chunk 
+bigger_bit=`echo "($SLURM_ARRAY_TASK_ID + 1) * ${SUB_SIZE}" | bc`
+
+
+N_SUBJECTS=$(( $( wc -l ${BIDS_DIR}/participants.tsv | cut -f1 -d' ' ) - 1 ))
+array_job_length=$(echo "$N_SUBJECTS/${SUB_SIZE}" | bc)
+Tail=$((N_SUBJECTS-(array_job_length*SUB_SIZE)))
+
+if [ "$SLURM_ARRAY_TASK_ID" -eq "$array_job_length" ]; then
+    SUBJECTS=`sed -n -E "s/sub-(\S*)\>.*/\1/gp" ${BIDS_DIR}/participants.tsv  | head -n ${N_SUBJECTS} | tail -n ${Tail}`
+else
+    SUBJECTS=`sed -n -E "s/sub-(\S*)\>.*/\1/gp" ${BIDS_DIR}/participants.tsv | head -n ${bigger_bit} | tail -n ${SUB_SIZE}`
+fi
+
+## set singularity environment variables that will point to the freesurfer license and the templateflow bits
+# export SINGULARITYENV_TEMPLATEFLOW_HOME=/home/fmriprep/.cache/templateflow
+# Make sure FS_LICENSE is defined in the container.
+export APPTAINERENV_FS_LICENSE=/home/freesurfer/.freesurfer.txt
+
+# # Remove IsRunning files from FreeSurfer
+# for subject in $SUBJECTS: do
+#     find ${LOCAL_FREESURFER_DIR}/sub-$subject/ -name "*IsRunning*" -type f -delete
+# done
+
+export ORIG_FS_LICENSE=${BASEDIR}/templates/.freesurfer.txt
+
+singularity run --cleanenv \
+    -B ${BASEDIR}/templates:/home/freesurfer --home /home/freesurfer \
+    -B ${BIDS_DIR}:/bids \
+    -B ${OUTPUT_DIR}:/derived \
+    -B ${ORIG_FS_LICENSE}:/li \
+    ${SING_CONTAINER} \
+    /bids /derived participant \
+    --participant_label ${SUBJECTS} \
+    --skip_bids_validator \
+    --license_file /li \
+    --n_cpus 80  
+
+# tip: add this line to the above command if skull stripping has already been done
+#   --skull-strip-t1w force \ # uncomment this line if skull stripping has aleady been done
+exitcode=$?
+
+
+# Output results to a table
+for subject in $SUBJECTS; do
+    if [ $exitcode -eq 0 ]; then
+        echo "sub-$subject   ${SLURM_ARRAY_TASK_ID}    0" \
+            >> ${LOGS_DIR}/${SLURM_JOB_NAME}.${SLURM_ARRAY_JOB_ID}.tsv
+    else
+        echo "sub-$subject   ${SLURM_ARRAY_TASK_ID}    freesurfer failed" \
+            >> ${LOGS_DIR}/${SLURM_JOB_NAME}.${SLURM_ARRAY_JOB_ID}.tsv
+    fi
+done
