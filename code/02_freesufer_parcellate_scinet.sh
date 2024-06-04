18a209bba0e894bb1484b612c013ce494bddd0f8
GhazalehManj <126309136+GhazalehManj@users.noreply.github.com>
2024-06-03 21:29:13 -0400

    Update 02_freesufer_parcellate_scinet.sh

--- code/02_freesufer_parcellate_scinet.sh
+++ code/02_freesufer_parcellate_scinet.sh
@@ -62,31 +62,32 @@ for subject in "${SUBJECTS[@]}"; do
         '-B', f'{GCS_FILE_DIR}:/gcs_files',
         SING_CONTAINER,
         '/bin/bash', '-c',
-        f'''
-        export SUBJECTS_DIR=/subjects
-
-        # List all lh and rh GCS files in the directory
-        LH_GCS_FILES=(/gcs_files/lh.*.gcs)
-        RH_GCS_FILES=(/gcs_files/rh.*.gcs)
-
-        for subject in {SUBJECTS[@]}; do
-            for lh_gcs_file in ${{LH_GCS_FILES[@]}}; do
-                base_name=\$(basename \$lh_gcs_file .gcs)
-                mris_ca_label -l \$SUBJECTS_DIR/\$subject/label/lh.cortex.label \
-                    \$subject lh \$SUBJECTS_DIR/\$subject/surf/lh.sphere.reg \
-                    \$lh_gcs_file \
-                    \$SUBJECTS_DIR/\$subject/label/\${{base_name}}_order.annot
-            done
-
-            for rh_gcs_file in ${{RH_GCS_FILES[@]}}; do
-                base_name=\$(basename \$rh_gcs_file .gcs)
-                mris_ca_label -l \$SUBJECTS_DIR/\$subject/label/rh.cortex.label \
-                    \$subject rh \$SUBJECTS_DIR/\$subject/surf/rh.sphere.reg \
-                    \$rh_gcs_file \
-                    \$SUBJECTS_DIR/\$subject/label/\${{base_name}}_order.annot
-            done
-        done
         '''
+export SUBJECTS_DIR=/subjects
+
+# List all lh and rh GCS files in the directory
+LH_GCS_FILES=(/gcs_files/lh.*.gcs)
+RH_GCS_FILES=(/gcs_files/rh.*.gcs)
+
+for subject in "${SUBJECTS[@]}"; do
+    for lh_gcs_file in "${LH_GCS_FILES[@]}"; do
+        base_name=$(basename "$lh_gcs_file" .gcs)
+        mris_ca_label -l "$SUBJECTS_DIR/$subject/label/lh.cortex.label" \
+            "$subject" lh "$SUBJECTS_DIR/$subject/surf/lh.sphere.reg" \
+            "$lh_gcs_file" \
+            "$SUBJECTS_DIR/$subject/label/${base_name}_order.annot"
+    done
+
+    for rh_gcs_file in "${RH_GCS_FILES[@]}"; do
+        base_name=$(basename "$rh_gcs_file" .gcs)
+        mris_ca_label -l "$SUBJECTS_DIR/$subject/label/rh.cortex.label" \
+            "$subject" rh "$SUBJECTS_DIR/$subject/surf/rh.sphere.reg" \
+            "$rh_gcs_file" \
+            "$SUBJECTS_DIR/$subject/label/${base_name}_order.annot"
+    done
+done
+'''
+
     ])
 "
 # Capture the exit code of the Python script
