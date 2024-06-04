ab8b50b57fbf8a85ed545f0bc9891cac72410d5c
GhazalehManj <126309136+GhazalehManj@users.noreply.github.com>
2024-06-04 10:25:09 -0400

    Update 02_freesufer_parcellate_scinet.sh

--- code/02_freesufer_parcellate_scinet.sh
+++ code/02_freesufer_parcellate_scinet.sh
@@ -1,26 +1,3 @@
-#!/bin/bash
-#SBATCH --job-name=freesurfer_parcellate
-#SBATCH --output=logs/%x_%j.out 
-#SBATCH --nodes=1
-#SBATCH --cpus-per-task=40
-#SBATCH --time=1:00:00
-
-SUB_SIZE=1  # Number of subjects to run per job
-
-# Set BASEDIR to the submission directory
-BASEDIR=${SLURM_SUBMIT_DIR}
-
-# Cleanup function to clear the ramdisk
-function cleanup_ramdisk {
-    echo -n "Cleaning up ramdisk directory /$SLURM_TMPDIR/ on "
-    date
-    rm -rf /$SLURM_TMPDIR
-    echo -n "done at "
-    date
-}
-
-trap "cleanup_ramdisk" TERM
-
 # Set environment variables
 export BIDS_DIR=${BASEDIR}/data/local/bids
 export FMRIPREP_HOME=${BASEDIR}/templates
@@ -35,18 +12,6 @@ export GCS_FILE_DIR=${BASEDIR}/templates/freesurfer_parcellate
 # Create necessary directories
 mkdir -vp ${OUTPUT_DIR} ${LOGS_DIR}
 
-# Calculate subject list for this job array
-N_SUBJECTS=$(( $(wc -l < ${BIDS_DIR}/participants.tsv) - 1 ))
-bigger_bit=$(( (SLURM_ARRAY_TASK_ID + 1) * SUB_SIZE ))
-array_job_length=$(( N_SUBJECTS / SUB_SIZE ))
-Tail=$(( N_SUBJECTS - (array_job_length * SUB_SIZE) ))
-
-if [ "$SLURM_ARRAY_TASK_ID" -eq "$array_job_length" ]; then
-    SUBJECTS=$(sed -n -E 's/sub-(\S*)\>.*/\1/gp' ${BIDS_DIR}/participants.tsv | head -n ${N_SUBJECTS} | tail -n ${Tail})
-else
-    SUBJECTS=$(sed -n -E 's/sub-(\S*)\>.*/\1/gp' ${BIDS_DIR}/participants.tsv | head -n ${bigger_bit} | tail -n ${SUB_SIZE})
-fi
-
 singularity exec \
     -B ${BASEDIR}/templates:/home/freesurfer --home /home/freesurfer \
     -B ${BIDS_DIR}:/bids \
@@ -54,30 +19,36 @@ singularity exec \
     -B ${ORIG_FS_LICENSE}:/li \
     -B ${SUBJECTS_DIR}:/subjects_dir \
     -B ${GCS_FILE_DIR}:/gcs_files \
-    ${SING_CONTAINER} /bin/bash
-    << "EOF"
-    
+    ${SING_CONTAINER} /bin/bash << "EOF"
+
+      # Read the subjects from participants.tsv
+      SUBJECTS_FILE=/bids/participants.tsv
+      SUBJECTS=$(tail -n +2 $SUBJECTS_FILE | cut -f1)
+
       SUBJECTS_DIR=/subjects_dir
 
       # List all lh and rh GCS files in the directory
       LH_GCS_FILES=(/gcs_files/lh.*.gcs)
       RH_GCS_FILES=(/gcs_files/rh.*.gcs)
 
-      for lh_gcs_file in "${LH_GCS_FILES[@]}"; do
-        base_name=$(basename $lh_gcs_file.gcs)
-        mris_ca_label -l $SUBJECTS_DIR/sub-$SUBJECTS/label/lh.cortex.label \
-        sub-$SUBJECTS lh $SUBJECTS_DIR/sub-$SUBJECTS/surf/lh.sphere.reg \
-        $lh_gcs_file \
-        $SUBJECTS_DIR/sub-$SUBJECTS/label/${base_name}_order.annot
-      done 
+      # Loop over each subject
+      for SUBJECT in $SUBJECTS; do
+        for lh_gcs_file in "${LH_GCS_FILES[@]}"; do
+          base_name=$(basename $lh_gcs_file .gcs)
+          mris_ca_label -l $SUBJECTS_DIR/$SUBJECT/label/lh.cortex.label \
+          $SUBJECT lh $SUBJECTS_DIR/$SUBJECT/surf/lh.sphere.reg \
+          $lh_gcs_file \
+          $SUBJECTS_DIR/$SUBJECT/label/${base_name}_order.annot
+        done 
 
-     for rh_gcs_file in "${RH_GCS_FILES[@]}"; do
-        base_name=$(basename $rh_gcs_file.gcs)
-        mris_ca_label -l $SUBJECTS_DIR/sub-$SUBJECTS/label/rh.cortex.label \
-        sub-$SUBJECTS rh $SUBJECTS_DIR/sub-$SUBJECTS/surf/rh.sphere.reg \
-        $rh_gcs_file \
-        $SUBJECTS_DIR/sub-$SUBJECTS/label/${base_name}_order.annot
-     done
-     
+        for rh_gcs_file in "${RH_GCS_FILES[@]}"; do
+          base_name=$(basename $rh_gcs_file .gcs)
+          mris_ca_label -l $SUBJECTS_DIR/$SUBJECT/label/rh.cortex.label \
+          $SUBJECT rh $SUBJECTS_DIR/$SUBJECT/surf/rh.sphere.reg \
+          $rh_gcs_file \
+          $SUBJECTS_DIR/$SUBJECT/label/${base_name}_order.annot
+        done
+        
+      done
+
 EOF
-
