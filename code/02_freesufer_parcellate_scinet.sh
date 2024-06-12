c0a4a1ad50084f2302a115966a9e9cc84dc77801
GhazalehManj <126309136+GhazalehManj@users.noreply.github.com>
2024-06-12 13:22:37 -0400

    Update 02_freesufer_parcellate_scinet.sh

--- code/02_freesufer_parcellate_scinet.sh
+++ code/02_freesufer_parcellate_scinet.sh
@@ -80,6 +80,10 @@ singularity exec \
           $rh_gcs_file \
           $SUBJECTS_DIR/$SUBJECT/label/${base_name}_order.annot
         done
+
+        #for N
+        # mri_aparc2aseg --s $SUBJECT--o output.mgz --annot Schaefer2018_${N}Parcels_7Networks_order
+        #done
         
       done
 
