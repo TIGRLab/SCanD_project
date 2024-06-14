cdc52399e524f1d2367f4915adf79ac1e1711616
GhazalehManj <126309136+GhazalehManj@users.noreply.github.com>
2024-06-14 15:00:53 -0400

    Update 02_freesurfer_group_scinet.sh

--- code/02_freesurfer_group_scinet.sh
+++ code/02_freesurfer_group_scinet.sh
@@ -3,9 +3,11 @@
 #SBATCH --output=logs/%x_%j.out 
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=40
-#SBATCH --time=3:00:00
+#SBATCH --time=00:30:00
+
 
 SUB_SIZE=1 ## number of subjects to run
+
 
 ####----### the next bit only works IF this script is submitted from the $BASEDIR/$OPENNEURO_DS folder...
 
@@ -25,7 +27,6 @@ function cleanup_ramdisk {
 # that happens, so results may be saved.
 trap "cleanup_ramdisk" TERM
 
-# input is BIDS_DIR this is where the data downloaded from openneuro went
 export BIDS_DIR=${BASEDIR}/data/local/bids
 export FMRIPREP_HOME=${BASEDIR}/templates
 export SING_CONTAINER=${BASEDIR}/containers/freesurfer-7.4.1.simg
@@ -33,11 +34,24 @@ export OUTPUT_DIR=${BASEDIR}/data/local/freesurfer_long  # use if version of fmr
 export LOGS_DIR=${BASEDIR}/logs
 mkdir -vp ${OUTPUT_DIR} ${LOGS_DIR} # ${LOCAL_FREESURFER_DIR}
 
-SUBJECTS=$(sed -n -E "s/sub-(\S*).*/\1/p" ${BIDS_DIR}/participants.tsv)
+## get the subject list from a combo of the array id, the participants.tsv and the chunk 
+bigger_bit=`echo "($SLURM_ARRAY_TASK_ID + 1) * ${SUB_SIZE}" | bc`
 
-    
+
+N_SUBJECTS=$(( $( wc -l ${BIDS_DIR}/participants.tsv | cut -f1 -d' ' ) - 1 ))
+array_job_length=$(echo "$N_SUBJECTS/${SUB_SIZE}" | bc)
+Tail=$((N_SUBJECTS-(array_job_length*SUB_SIZE)))
+
+if [ "$SLURM_ARRAY_TASK_ID" -eq "$array_job_length" ]; then
+    SUBJECTS=`sed -n -E "s/sub-(\S*)\>.*/\1/gp" ${BIDS_DIR}/participants.tsv  | head -n ${N_SUBJECTS} | tail -n ${Tail}`
+else
+    SUBJECTS=`sed -n -E "s/sub-(\S*)\>.*/\1/gp" ${BIDS_DIR}/participants.tsv | head -n ${bigger_bit} | tail -n ${SUB_SIZE}`
+fi
+
+
 export APPTAINERENV_FS_LICENSE=/home/freesurfer/.freesurfer.txt
 export ORIG_FS_LICENSE=${BASEDIR}/templates/.freesurfer.txt
+
 
  singularity run --cleanenv \
     -B ${BASEDIR}/templates:/home/freesurfer --home /home/freesurfer \
@@ -52,111 +66,3 @@ export ORIG_FS_LICENSE=${BASEDIR}/templates/.freesurfer.txt
     --license_file /li \
     --n_cpus 80
 
-
-
-SUBJECTS=$(cut -f 1 ${BASEDIR}/data/local/bids/participants.tsv | tail -n +2)
-
-# Iterate over each subject in SUBJECTS
-for subject in $SUBJECTS; do
-    echo "$subject       0" >> ${BASEDIR}/logs/freesurfer_group.tsv
-done
-
-
-export SUBJECTS_DIR=${BASEDIR}/data/local/freesurfer_long
-export GCS_FILE_DIR=${BASEDIR}/templates/freesurfer_parcellate
-
-
-singularity exec \
-    -B ${BASEDIR}/templates:/home/freesurfer --home /home/freesurfer \
-    -B ${BIDS_DIR}:/bids \
-    -B ${ORIG_FS_LICENSE}:/li \
-    -B ${SUBJECTS_DIR}:/subjects_dir \
-    -B ${GCS_FILE_DIR}:/gcs_files \
-    ${SING_CONTAINER} /bin/bash << "EOF"
-
-      # Read the subjects from participants.tsv
-      SUBJECTS_FILE=/bids/participants.tsv
-      SUBJECTS=$(tail -n +2 $SUBJECTS_FILE | cut -f1)
-
-      SUBJECTS_DIR=/subjects_dir
-
-      # List all lh and rh GCS files in the directory
-      LH_GCS_FILES=(/gcs_files/lh.*.gcs)
-      RH_GCS_FILES=(/gcs_files/rh.*.gcs)
-
-      # Loop over each subject
-      for SUBJECT in $SUBJECTS; do
-      
-        SUBJECT_LONG_DIRS=$(find $SUBJECTS_DIR -maxdepth 1 -name "${SUBJECT}*.long.${SUBJECT}" -type d)
-        
-        for SUBJECT_LONG_DIR in $SUBJECT_LONG_DIRS; do
-          sub=$(basename $SUBJECT_LONG_DIR)
-    
-          for lh_gcs_file in "${LH_GCS_FILES[@]}"; do
-            base_name=$(basename $lh_gcs_file .gcs)
-            mris_ca_label -l $SUBJECT_LONG_DIR/label/lh.cortex.label \
-            $sub lh $SUBJECT_LONG_DIR/surf/lh.sphere.reg \
-            $lh_gcs_file \
-            $SUBJECT_LONG_DIR/label/${base_name}_order.annot
-          done 
-
-          for rh_gcs_file in "${RH_GCS_FILES[@]}"; do
-            base_name=$(basename $rh_gcs_file .gcs)
-            mris_ca_label -l $SUBJECT_LONG_DIR/label/rh.cortex.label \
-            $sub rh $SUBJECT_LONG_DIR/surf/rh.sphere.reg \
-            $rh_gcs_file \
-            $SUBJECT_LONG_DIR/label/${base_name}_order.annot
-          done
-
-          for N in {1,2,3,4,5,6,7,8,9,10};do 
-            mri_aparc2aseg --s $sub --o $SUBJECT_LONG_DIR/label/output_${N}00Parcels.mgz --annot Schaefer2018_${N}00Parcels_7Networks_order
-
-            # Generate anatomical stats
-            mris_anatomical_stats -a $SUBJECT_LONG_DIR/label/lh.Schaefer2018_${N}00Parcels_7Networks_order.annot -f $SUBJECT_LONG_DIR/stats/lh.Schaefer2018_${N}00Parcels_7Networks_order.stats $sub lh
-            mris_anatomical_stats -a $SUBJECT_LONG_DIR/label/rh.Schaefer2018_${N}00Parcels_7Networks_order.annot -f $SUBJECT_LONG_DIR/stats/rh.Schaefer2018_${N}00Parcels_7Networks_order.stats $sub rh
-
-            # Extract stats to table format
-            aparcstats2table --subjects $sub --hemi lh --parc Schaefer2018_${N}00Parcels_7Networks_order --measure thickness --tablefile $SUBJECT_LONG_DIR/stats/lh.Schaefer2018_${N}00Parcels_table.tsv
-            aparcstats2table --subjects $sub --hemi rh --parc Schaefer2018_${N}00Parcels_7Networks_order --measure thickness --tablefile $SUBJECT_LONG_DIR/stats/rh.Schaefer2018_${N}00Parcels_table.tsv
-          done
-        
-        done
-     
-      done   
-
-EOF
-
-
-# Merging TSV files
-OUTPUT_MERGE_DIR=${SUBJECTS_DIR}/00_group2_stats_tables
-mkdir -p ${OUTPUT_MERGE_DIR}
-
-SUBJECTS_FILE=${BIDS_DIR}/participants.tsv
-SUBJECTS=$(tail -n +2 $SUBJECTS_FILE | cut -f1)
-
-
-for N in {1,2,3,4,5,6,7,8,9,10}; do
-  for hemi in lh rh; do
-    OUTPUT_FILE=${OUTPUT_MERGE_DIR}/${hemi}.Schaefer2018_${N}00Parcels.thickness.tsv
-    HEADER_ADDED=false
-
-    for subject in $SUBJECTS; do
-      SUBJECT_LONG_DIRS=$(find $SUBJECTS_DIR -maxdepth 1 -name "${subject}*.long.${subject}" -type d)
-      
-      for SUBJECT_LONG_DIR in $SUBJECT_LONG_DIRS; do
-        FILE=${SUBJECT_LONG_DIR}/stats/${hemi}.Schaefer2018_${N}00Parcels_table.tsv
-
-        if [ -f "$FILE" ]; then
-          if [ "$HEADER_ADDED" = false ]; then
-            head -n 1 $FILE > $OUTPUT_FILE
-            HEADER_ADDED=true
-          fi
-
-          tail -n +2 $FILE >> $OUTPUT_FILE
-        else
-          echo "File $FILE not found, skipping..."
-        fi
-      done
-    done
-  done
-done
