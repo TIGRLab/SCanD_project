f6e0011be157db53930dd4a99d4b94e8f3455bb6
GhazalehManj <126309136+GhazalehManj@users.noreply.github.com>
2024-08-27 17:43:58 -0400

    Update test_1.sh

--- code/test_1.sh
+++ code/test_1.sh
@@ -1,8 +1,8 @@
-export BIDS_DIR=$PROJECT/SCanD_project_GMANJ/data/local/bids
+export BIDS_DIR=$SCRATCH/SCanD_project/data/local/bids
 
 SUBJECTS=$(sed -n -E "s/sub-(\S*).*/\1/p" ${BIDS_DIR}/participants.tsv)
 
-export SUBJECTS_DIR=${BASEDIR}/data/local/freesurfer
+export SUBJECTS_DIR=${BASEDIR}/data/local/freesurfer_long
 
 # Merging TSV files
 OUTPUT_MERGE_DIR=${SUBJECTS_DIR}/00_group2_stats_tables
@@ -22,7 +22,7 @@ for N in {1..10}; do
       HEADER_ADDED=false
 
       for subject in $SUBJECTS; do
-        SUBJECT_LONG_DIRS=$(find $SUBJECTS_DIR -maxdepth 1 -name "${subject}*" -type d)
+        SUBJECT_LONG_DIRS=$(find $SUBJECTS_DIR -maxdepth 1 -name "${subject}*.long.${subject}" -type d)
 
         for SUBJECT_LONG_DIR in $SUBJECT_LONG_DIRS; do
           FILE=${SUBJECT_LONG_DIR}/stats/${hemi}.Schaefer2018_${N}00Parcels_table_${type}.tsv
