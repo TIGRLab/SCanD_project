b4018d4e860f57c74ec5a0e0aedd234bb4e73dc6
GhazalehManj <126309136+GhazalehManj@users.noreply.github.com>
2024-09-10 22:44:39 -0400

    Update 02_freesurfer_group_scinet.sh

--- code/02_freesurfer_group_scinet.sh
+++ code/02_freesurfer_group_scinet.sh
@@ -3,7 +3,7 @@
 #SBATCH --output=logs/%x_%j.out 
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=40
-#SBATCH --time=05:00:00
+#SBATCH --time=06:00:00
 
 
 ## set the second environment variable to get the base directory
@@ -29,7 +29,7 @@ export ORIG_FS_LICENSE=${BASEDIR}/templates/.freesurfer.txt
 export SUBJECTS_DIR=${BASEDIR}/data/local/derivatives/freesurfer/7.4.1
 export GCS_FILE_DIR=${BASEDIR}/templates/freesurfer_parcellate
 
-SUB_SIZE=5
+SUB_SIZE=3
 
 bigger_bit=`echo "($SLURM_ARRAY_TASK_ID + 1) * ${SUB_SIZE}" | bc`
 
