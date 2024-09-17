9cfbfefc892d01b4fa2f5362239114b063116ebe
GhazalehManj <126309136+GhazalehManj@users.noreply.github.com>
2024-09-17 17:39:42 -0400

    Update 05_extract_to_share.sh

--- code/05_extract_to_share.sh
+++ code/05_extract_to_share.sh
@@ -204,3 +204,22 @@ source ./code/freesurfer_group_merge.sh
 echo "copying over freesurfer group files"
 mkdir ${PROJECT_DIR}/data/share/freesurfer_group
 rsync -a ${PROJECT_DIR}/data/local/derivatives/freesurfer/7.4.1/00_group2_stats_tables/*  ${PROJECT_DIR}/data/share/freesurfer_group
+
+
+
+## Generate qsiprep motion metrics and extract NODDI indices
+module load NiaEnv/2019b python/3.6.8
+
+# Create a directory for virtual environments if it doesn't exist
+mkdir ~/.virtualenvs
+cd ~/.virtualenvs
+virtualenv --system-site-packages ~/.virtualenvs/myenv
+
+# Activate the virtual environment
+source ~/.virtualenvs/myenv/bin/activate 
+
+cd ${PROJECT_DIR}
+python3 code/gen_qsiprep_motion_metrics.py
+
+python3 -m pip install nilearn
+python3 code/extract_NODDI_indices.py data/local/derivatives/qsiprep/0.21.4/qsiprep/  data/local/derivatives/qsiprep/0.22.0/amico_noddi/qsirecon-NODDI data/local/derivatives/qsiprep/0.22.0/amico_noddi
