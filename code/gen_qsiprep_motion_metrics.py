30eb714d23aef3067436ef6c37977d57f15da281
GhazalehManj <126309136+GhazalehManj@users.noreply.github.com>
2024-09-17 18:12:41 -0400

    Update gen_qsiprep_motion_metrics.py

--- code/gen_qsiprep_motion_metrics.py
+++ code/gen_qsiprep_motion_metrics.py
@@ -9,7 +9,7 @@ scratch_dir = os.getenv('SCRATCH')
 search_pattern = f"{scratch_dir}/SCanD_project/data/local/derivatives/qsiprep/0.22.0/qsiprep/*/*/dwi/*desc-ImageQC_dwi.csv"
 
 # Get list of all relevant CSV files
-tay_dwi_metrics_files = glob.glob(search_pattern, recursive=True)
+dwi_metrics_files = glob.glob(search_pattern, recursive=True)
 
 # Function to read each CSV and add the filename (without .csv extension)
 def read_and_add_filename(filepath):
@@ -18,16 +18,16 @@ def read_and_add_filename(filepath):
     return df
 
 # Read all CSV files into a single DataFrame
-tay_dwi_metrics = pd.concat([read_and_add_filename(f) for f in tay_dwi_metrics_files], ignore_index=True)
+dwi_metrics = pd.concat([read_and_add_filename(f) for f in dwi_metrics_files], ignore_index=True)
 
 # Separate 'filename' column into 'subject' and 'session'
 # Extract subject and session from the filename
-tay_dwi_metrics['subject'] = tay_dwi_metrics['filename'].str.split('_').str[0].replace('sub-', '')
-tay_dwi_metrics['session'] = tay_dwi_metrics['filename'].str.split('_').str[1].replace('ses-', '')
+dwi_metrics['subject'] = dwi_metrics['filename'].str.split('_').str[0].replace('sub-', '')
+dwi_metrics['session'] = dwi_metrics['filename'].str.split('_').str[1].replace('ses-', '')
 
 
 output_dir = f"{scratch_dir}/SCanD_project/data/local/derivatives/qsiprep/0.22.0/qsiprep"
 
 # Write the combined DataFrame to a new CSV file
 output_file = os.path.join(output_dir, "qsiprep_metrics.csv")
-tay_dwi_metrics.to_csv(output_file, index=False)
+dwi_metrics.to_csv(output_file, index=False)
