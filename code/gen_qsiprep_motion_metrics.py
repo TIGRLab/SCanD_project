3986356f5db25fd627bc63293c0eb1b261ad670c
GhazalehManj <126309136+GhazalehManj@users.noreply.github.com>
2024-09-17 16:29:02 -0400

    Update gen_qsiprep_motion_metrics.py

--- code/gen_qsiprep_motion_metrics.py
+++ code/gen_qsiprep_motion_metrics.py
@@ -2,9 +2,14 @@ import pandas as pd
 import os
 import glob
 
-# Get list of all relevant CSV files
+# Get the SCRATCH directory
 scratch_dir = os.getenv('SCRATCH')
-tay_dwi_metrics_files = glob.glob("$SCRATCH/SCanD_project_test/data/local/derivatives/qsiprep/0.22.0/qsiprep/*/*/dwi/*desc-ImageQC_dwi.csv", recursive=True)
+
+# Expand the path with the SCRATCH directory
+search_pattern = f"{scratch_dir}/SCanD_project_test/data/local/derivatives/qsiprep/0.22.0/qsiprep/*/*/dwi/*desc-ImageQC_dwi.csv"
+
+# Get list of all relevant CSV files
+tay_dwi_metrics_files = glob.glob(search_pattern, recursive=True)
 
 # Function to read each CSV and add the filename (without .csv extension)
 def read_and_add_filename(filepath):
@@ -12,11 +17,17 @@ def read_and_add_filename(filepath):
     df['filename'] = os.path.basename(filepath).replace('.csv', '')
     return df
 
-# Read all CSV files into a single dataframe
+# Read all CSV files into a single DataFrame
 tay_dwi_metrics = pd.concat([read_and_add_filename(f) for f in tay_dwi_metrics_files], ignore_index=True)
 
 # Separate 'filename' column into 'subject' and 'session'
-tay_dwi_metrics[['subject', 'session']] = tay_dwi_metrics['filename'].str.split('_', expand=True)
+# Extract subject and session from the filename
+tay_dwi_metrics['subject'] = tay_dwi_metrics['filename'].str.split('_').str[0].replace('sub-', '')
+tay_dwi_metrics['session'] = tay_dwi_metrics['filename'].str.split('_').str[1].replace('ses-', '')
 
-# Write the combined dataframe to a new CSV file
-tay_dwi_metrics.to_csv("$SCRATCH/SCanD_project_test/data/local/derivatives/qsiprep/0.22.0/qsiprep/qsiprep_metrics.csv", index=False)
+
+output_dir = f"{scratch_dir}/SCanD_project_test/data/local/derivatives/qsiprep/0.22.0/qsiprep"
+
+# Write the combined DataFrame to a new CSV file
+output_file = os.path.join(output_dir, "qsiprep_metrics.csv")
+tay_dwi_metrics.to_csv(output_file, index=False)
