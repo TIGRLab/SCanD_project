d574329fbbba7593bf204f42e0034a4a358b28a6
GhazalehManj <126309136+GhazalehManj@users.noreply.github.com>
2024-10-02 20:12:45 -0400

    Update 01_mriqc_scinet.sh

--- code/01_mriqc_scinet.sh
+++ code/01_mriqc_scinet.sh
@@ -6,7 +6,7 @@
 #SBATCH --time=18:00:00
 
 
-SUB_SIZE=4 ## number of subjects to run
+SUB_SIZE=1 ## number of subjects to run
 export THREADS_PER_COMMAND=2
 
 ####----### the next bit only works IF this script is submitted from the $BASEDIR/$OPENNEURO_DS folder...
