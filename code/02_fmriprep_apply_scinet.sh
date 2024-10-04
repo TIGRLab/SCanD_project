14d95565ab7f1df94f4f9aac4b79b0dea3ccb494
GhazalehManj <126309136+GhazalehManj@users.noreply.github.com>
2024-10-04 15:29:51 -0400

    Update and rename 02_fmriprep_func_scinet.sh to 02_fmriprep_apply_scinet.sh

--- code/02_fmriprep_apply_scinet.sh
+++ code/02_fmriprep_apply_scinet.sh
@@ -92,7 +92,7 @@ for subject in $SUBJECTS; do
         echo "sub-$subject   ${SLURM_ARRAY_TASK_ID}    0" \
             >> ${LOGS_DIR}/${SLURM_JOB_NAME}.${SLURM_ARRAY_JOB_ID}.tsv
     else
-        echo "sub-$subject   ${SLURM_ARRAY_TASK_ID}    fmriprep_func failed" \
+        echo "sub-$subject   ${SLURM_ARRAY_TASK_ID}    fmriprep_apply failed" \
             >> ${LOGS_DIR}/${SLURM_JOB_NAME}.${SLURM_ARRAY_JOB_ID}.tsv
     fi
 done
