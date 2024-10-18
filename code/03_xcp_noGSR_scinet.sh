326deea54611527045e616a16821a01a76e96f91
GhazalehManj <126309136+GhazalehManj@users.noreply.github.com>
2024-10-18 11:09:51 -0400

    Update 03_xcp_noGSR_scinet.sh

--- code/03_xcp_noGSR_scinet.sh
+++ code/03_xcp_noGSR_scinet.sh
@@ -3,7 +3,7 @@
 #SBATCH --output=logs/%x_%j.out
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=80
-#SBATCH --time=10:00:00
+#SBATCH --time=05:00:00
 
 
 SUB_SIZE=1 ## number of subjects to run is 1 because there are multiple tasks/run that will run in parallel
