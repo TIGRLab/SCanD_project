68ec4a942a24346dfa7ab145241b3b0ac780fb85
GhazalehManj <126309136+GhazalehManj@users.noreply.github.com>
2024-10-30 12:53:11 -0400

    Update test.sh

--- code/test.sh
+++ code/test.sh
@@ -1,1 +1,6 @@
 
+singularity exec --cleanenv \
+  -B ${PROJECT_DIR}/data/local/bids:/bids \
+  -B ${PROJECT_DIR}/data/local/derivatives/ciftify:/derived \
+  ${PROJECT_DIR}/containers/fmriprep_ciftity-v1.3.2-2.3.3.simg \
+  cifti_vis_recon_all index --ciftify-work-dir /derived
