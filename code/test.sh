a8e8db6d07c0bf02a433487ab82b8789ab7f10a7
GhazalehManj <126309136+GhazalehManj@users.noreply.github.com>
2024-10-30 13:03:45 -0400

    Update test.sh

--- code/test.sh
+++ code/test.sh
@@ -1,6 +1,93 @@
+#!/bin/bash
+#SBATCH --job-name=smriprep
+#SBATCH --output=logs/%x_%j.out 
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=40
+#SBATCH --time=10:00:00
+
+
+SUB_SIZE=1 ## number of subjects to run
+CORES=40
+export THREADS_PER_COMMAND=2
+
+####----### the next bit only works IF this script is submitted from the $BASEDIR/$OPENNEURO_DS folder...
+
+## set the second environment variable to get the base directory
+BASEDIR=${SLURM_SUBMIT_DIR}
+
+## set up a trap that will clear the ramdisk if it is not cleared
+function cleanup_ramdisk {
+    echo -n "Cleaning up ramdisk directory /$SLURM_TMPDIR/ on "
+    date
+    rm -rf /$SLURM_TMPDIR
+    echo -n "done at "
+    date
+}
+
+#trap the termination signal, and call the function 'trap_term' when
+# that happens, so results may be saved.
+trap "cleanup_ramdisk" TERM
+
+
+# input is BIDS_DIR this is where the data downloaded from openneuro went
+export BIDS_DIR=${BASEDIR}/data/local/bids
+
+## these folders envs need to be set up for this script to run properly 
+## see notebooks/00_setting_up_envs.md for the set up instructions
+export SMRIPREP_HOME=${BASEDIR}/templates
+export SING_CONTAINER=${BASEDIR}/containers/fmriprep-23.2.3.simg
+
+
+## setting up the output folders
+export OUTPUT_DIR=${BASEDIR}/data/local/derivatives/smriprep/23.2.3 
+
+# export LOCAL_FREESURFER_DIR=${SCRATCH}/${STUDY}/data/derived/freesurfer-6.0.1
+project_id=$(cat ${BASEDIR}/project_id)
+export WORK_DIR=${BBUFFER}/SCanD/${project_id}/smriprep
+export LOGS_DIR=${BASEDIR}/logs
+mkdir -vp ${OUTPUT_DIR} ${WORK_DIR} # ${LOCAL_FREESURFER_DIR}
+
+## get the subject list from a combo of the array id, the participants.tsv and the chunk 
+bigger_bit=`echo "($SLURM_ARRAY_TASK_ID + 1) * ${SUB_SIZE}" | bc`
+
+N_SUBJECTS=$(( $( wc -l ${BIDS_DIR}/participants.tsv | cut -f1 -d' ' ) - 1 ))
+array_job_length=$(echo "$N_SUBJECTS/${SUB_SIZE}" | bc)
+Tail=$((N_SUBJECTS-(array_job_length*SUB_SIZE)))
+
+if [ "$SLURM_ARRAY_TASK_ID" -eq "$array_job_length" ]; then
+    SUBJECTS=`sed -n -E "s/sub-(\S*)\>.*/\1/gp" ${BIDS_DIR}/participants.tsv  | head -n ${N_SUBJECTS} | tail -n ${Tail}`
+else
+    SUBJECTS=`sed -n -E "s/sub-(\S*)\>.*/\1/gp" ${BIDS_DIR}/participants.tsv | head -n ${bigger_bit} | tail -n ${SUB_SIZE}`
+fi
+
+export APPTAINERENV_FS_LICENSE=/home/fmriprep/.freesurfer.txt
 
 singularity exec --cleanenv \
-  -B ${PROJECT_DIR}/data/local/bids:/bids \
-  -B ${PROJECT_DIR}/data/local/derivatives/ciftify:/derived \
-  ${PROJECT_DIR}/containers/fmriprep_ciftity-v1.3.2-2.3.3.simg \
-  cifti_vis_recon_all index --ciftify-work-dir /derived
+    -B ${BASEDIR}/templates:/home/fmriprep --home /home/fmriprep \
+    -B ${BIDS_DIR}:/bids \
+    -B ${OUTPUT_DIR}:/derived \
+    -B ${WORK_DIR}:/work \
+    ${SING_CONTAINER} \
+    smriprep
+    /bids /derived participant \
+    --participant_label ${SUBJECTS} \
+    -w /work \
+    --skip-bids-validation \
+    --omp-nthreads 8 \
+    --nthreads 40 \
+    --mem-mb 15000 \
+    --notrack 
+
+exitcode=$?
+
+
+# Output results to a table
+for subject in $SUBJECTS; do
+    if [ $exitcode -eq 0 ]; then
+        echo "sub-$subject   ${SLURM_ARRAY_TASK_ID}    0" \
+            >> ${LOGS_DIR}/${SLURM_JOB_NAME}.${SLURM_ARRAY_JOB_ID}.tsv
+    else
+        echo "sub-$subject   ${SLURM_ARRAY_TASK_ID}    smriprep_anat failed" \
+            >> ${LOGS_DIR}/${SLURM_JOB_NAME}.${SLURM_ARRAY_JOB_ID}.tsv
+    fi
+done
