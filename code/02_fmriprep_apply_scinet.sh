626937051baaae7fed02f7116e1748e02732a059
GhazalehManj <126309136+GhazalehManj@users.noreply.github.com>
2025-02-07 14:14:51 -0500

    Update 02_fmriprep_apply_scinet.sh

--- code/02_fmriprep_apply_scinet.sh
+++ code/02_fmriprep_apply_scinet.sh
@@ -3,7 +3,7 @@
 #SBATCH --output=logs/%x_%j.out 
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=16
-#SBATCH --time=03:00:00
+#SBATCH --time=06:00:00
 #SBATCH --mem-per-cpu=60000
 
 SUB_SIZE=1 ## number of subjects to run is 1 because there are multiple tasks/run that will run in parallel 
