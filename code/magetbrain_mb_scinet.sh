222d326ff1daa06cc204c74b01a8acb08cb02eae
GhazalehManj <126309136+GhazalehManj@users.noreply.github.com>
2025-02-12 15:36:46 -0500

    Update magetbrain_mb_scinet.sh

--- code/magetbrain_mb_scinet.sh
+++ code/magetbrain_mb_scinet.sh
@@ -1,1 +1,26 @@
+#!/bin/bash
+#SBATCH --job-name=maget_brain
+#SBATCH --output=log/%x_%j.out 
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=16
+#SBATCH --time=16:00:00
+#SBATCH --mem-per-cpu=60000
 
+module load apptainer/1.3.5
+
+LOG_DIR=$SCRATCH/SCanD_project/data/local/MAGeTbrain/log
+DATA_DIR=$SCRATCH/data/local/MAGeTbrain/magetbrain_data
+SING_CONTAINER=$SCRATCH/SCanD_project/containers/magetbrain.sif
+
+mkdir -p $LOG_DIR
+
+cd ~
+
+singularity run \
+   -B ${DATA_DIR}:/data \
+   ${SING_CONTAINER} \
+    mb --input_dir /data/input \
+       --output_dir /data/output \
+       --reg_dir /data/output/registration \
+       --save \
+       run
