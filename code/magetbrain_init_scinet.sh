dd44fbb1fc1fd59fa2a35d50b2acb43d4eca764e
GhazalehManj <126309136+GhazalehManj@users.noreply.github.com>
2025-02-12 15:39:48 -0500

    Update magetbrain_init_scinet.sh

--- code/magetbrain_init_scinet.sh
+++ code/magetbrain_init_scinet.sh
@@ -1,1 +1,10 @@
 
+mkdir magetbrain_data
+
+cp -r /archive/data/PREDICTS/data/bids/sub-CMH00000098/ses-01/anat/sub-CMH00000098_ses-01_T1w.nii.gz  magetbrain_data/input/subjects/brains/
+
+gunzip magetbrain_data/input/subjects/brains/sub-CMH00000098_ses-01_T1w.nii.gz
+
+
+nii2mnc magetbrain_data/input/subjects/brains/sub-CMH00000098_ses-01_T1w.nii \
+        magetbrain_data/input/subjects/brains/sub-CMH00000098_ses-01_T1w.mnc
