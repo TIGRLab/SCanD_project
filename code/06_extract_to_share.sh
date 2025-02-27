d4648c7fa2ad10329c3d1119de52ede0fc46abb4
GhazalehManj <126309136+GhazalehManj@users.noreply.github.com>
2025-02-27 16:42:47 -0500

    Update 06_extract_to_share.sh

--- code/06_extract_to_share.sh
+++ code/06_extract_to_share.sh
@@ -298,3 +298,12 @@ python3 code/gen_qsiprep_motion_metrics.py
 
 rsync -a ${PROJECT_DIR}/data/local/derivatives/qsiprep/0.22.0/qsiprep/qsiprep_metrics.csv ${PROJECT_DIR}/data/share/qsiprep/0.22.0/
 rsync -a ${PROJECT_DIR}/data/local/derivatives/qsiprep/0.22.0/amico_noddi/qsirecon-NODDI/noddi_roi ${PROJECT_DIR}/data/share/amico_noddi
+
+# sharing magetbrain outputs
+mkdir -p ${PROJECT_DIR}/data/share/magetbrain/input
+mkdir -p ${PROJECT_DIR}/data/share/magetbrain/output
+
+rsync -a ${PROJECT_DIR}/data/local/MAGeTbrain/magetbrain_data/output/fusion/majority_vote/*labels.mnc ${PROJECT_DIR}/data/share/magetbrain/output
+rsync -a ${PROJECT_DIR}/data/local/MAGeTbrain/MAGeTbrain/magetbrain_data/input/subjects/brains/*.mnc* ${PROJECT_DIR}/data/share/magetbrain/input
+
+
