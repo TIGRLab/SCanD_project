099f19078dcf6dffd449d69a0a1bb2762233ca86
GhazalehManj <126309136+GhazalehManj@users.noreply.github.com>
2025-03-12 12:20:02 -0400

    Update 01_magetbrain_init_scinet.sh

--- code/01_magetbrain_init_scinet.sh
+++ code/01_magetbrain_init_scinet.sh
@@ -21,6 +21,8 @@ INPUT_DIR="$MAGETBRAIN_DIR/input"
 # Create necessary directories
 mkdir -p "$INPUT_DIR/subjects/brains"
 mkdir -p "$INPUT_DIR/templates/brains"
+mkdir ~/.qbatch
+chmod -R 777 ~/.qbatch
 
 # Define the path to the demographic TSV file
 DEMOGRAPHIC_FILE="$PROJECT_DIR/data/local/bids/participants_demographic.tsv"
