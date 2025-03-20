1a14b0b6b9f556daf3aab1dbbf34623a8b82d9f1
GhazalehManj <126309136+GhazalehManj@users.noreply.github.com>
2025-03-19 21:30:25 -0400

    Update stage_3.sh

--- stage_3.sh
+++ stage_3.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-## stage 3 (xcp-d, xcp_noGSR, qsirecon_step2):
+## stage 3 (xcp-d, xcp_noGSR, qsirecon_step2, magetbrain_vote):
 
 submit_array_job() {
     local script=$1
@@ -25,7 +25,22 @@ run_pipeline() {
     fi
 }
 
+submit_magetbrain_job() {
+    local sub_size=1
+    local subjects_list=($(ls $PROJECT_DIR/data/local/MAGeTbrain/magetbrain_data/input/subjects/brains/*.mnc | xargs -n 1 basename | sed 's/\.mnc$//'))
+    local n_subjects=${#subjects_list[@]}
+    local array_job_length=$(echo "$n_subjects / ${sub_size}" | bc)
+    echo "Submitting MAGeTbrain Vote job with array size: ${array_job_length}"
+    sbatch --array=0-${array_job_length} code/3_magetbrain_vote_scinet.sh
+}
+
 # Prompt user for each pipeline
 run_pipeline "xcp-d" "code/03_xcp_scinet.sh" 1
 run_pipeline "xcp-noGSR" "code/03_xcp_noGSR_scinet.sh" 1
 run_pipeline "qsirecon_step2" "code/03_qsirecon_step2_scinet.sh" 1
+read -p "Do you want to run the MAGeTbrain_vote pipeline? (yes/no): " run_magetbrain
+if [[ "$run_magetbrain" =~ ^(yes|y)$ ]]; then
+    submit_magetbrain_job
+else
+    echo "Skipping MAGeTbrain_vote."
+fi
