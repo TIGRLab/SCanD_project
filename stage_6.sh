7859082a991911a452af7f825248b4f3447d380d
GhazalehManj <126309136+GhazalehManj@users.noreply.github.com>
2025-03-24 13:10:50 -0400

    Update stage_6.sh

--- stage_6.sh
+++ stage_6.sh
@@ -3,8 +3,8 @@
 read -p "Do you want to extract and share data? (yes/no): " run_share
 if [[ "$run_share" =~ ^(yes|y)$ ]]; then
     echo "Sharing data..."
-    sbatch ./code/06_extract_to_share1.sh
-    source ./code/06_extract_to_share2.sh
+    sbatch ./code/06_extract_to_share_slurm.sh
+    source ./code/06_extract_to_share_terminal.sh
 else
     echo "Skipping enigma_extract."
 fi
