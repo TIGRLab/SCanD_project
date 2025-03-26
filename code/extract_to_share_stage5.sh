e1163515373c351c78fe465b4c90bf8c286f6d13
GhazalehManj <126309136+GhazalehManj@users.noreply.github.com>
2025-03-26 12:37:55 -0400

    Create extract_to_share_stage5.sh

---
+++ code/extract_to_share_stage5.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#SBATCH --job-name=extract_to_share_stage5
+#SBATCH --output=logs/%x_%j.out
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=4
+#SBATCH --time=01:00:00
+#SBATCH --mem-per-cpu=1000
+
+
+# A script to extract the bits that we want to share back with the corsotium
+# meant to just be run one time after the other pipelines are run
+
+## copying the fmriprep QA files and figures plus logs and metadata to
+
+## extract-noddi
+
+module load apptainer/1.3.5
+
+PROJECT_DIR=${SLURM_SUBMIT_DIR}
+
+if [ -d "${PROJECT_DIR}/data/local/derivatives/qsiprep/0.22.0/amico_noddi/qsirecon-NODDI" ];
+then
+
+echo "copying over the extract_noddi folder"
+
+rsync -a --include='noddi_roi/' --include='noddi_roi/**/' --include='noddi_roi/**/*.png' --include='noddi_roi/**/*.csv' --exclude='noddi_roi/**' \
+    ${PROJECT_DIR}/data/local/derivatives/qsiprep/0.22.0/amico_noddi/qsirecon-NODDI/ \
+    ${PROJECT_DIR}/data/share/amico_noddi
+
+else
+
+    echo "No extract_noddi outputs found."
+
+fi
