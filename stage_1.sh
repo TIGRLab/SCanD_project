7e81b4000333578ea169843090601169f8a82343
GhazalehManj <126309136+GhazalehManj@users.noreply.github.com>
2025-03-27 16:08:32 -0400

    Update stage_1.sh

--- stage_1.sh
+++ stage_1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#stage1 (mriqc, fmriprep_fit, freesurfer, synthstrip, smriprep,magetbrain_init):
+#stage1 (mriqc, fmriprep_fit, freesurfer, synthstrip, smriprep, magetbrain_init):
 
 # Function to calculate and submit array jobs
 submit_array_job() {
