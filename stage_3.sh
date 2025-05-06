332d51fa491e899e2b1d68d82bba749ea6171f43
GhazalehManj <126309136+GhazalehManj@users.noreply.github.com>
2025-05-06 16:47:28 -0400

    Update stage_3.sh

--- stage_3.sh
+++ stage_3.sh
@@ -37,10 +37,10 @@ submit_magetbrain_job() {
 # Prompt user for each pipeline
 run_pipeline "xcp-d" "code/03_xcp_scinet.sh" 1
 run_pipeline "xcp-noGSR" "code/03_xcp_noGSR_scinet.sh" 1
-run_pipeline "qsirecon_step1" "./code/02_qsirecon_step1_scinet.sh" 1
-run_pipeline "amico_noddi" "./code/02_amico_noddi.sh" 1
-run_pipeline "tractography_multi shell" "./code/02_tractography_multi_scinet.sh" 1
-run_pipeline "tractography_single shell" "./code/02_tractography_single_scinet.sh" 1
+run_pipeline "qsirecon_step1" "./code/03_qsirecon_step1_scinet.sh" 1
+run_pipeline "amico_noddi" "./code/03_amico_noddi.sh" 1
+run_pipeline "tractography_multi shell" "./code/03_tractography_multi_scinet.sh" 1
+run_pipeline "tractography_single shell" "./code/03_tractography_single_scinet.sh" 1
 
 
 read -p "Do you want to run the MAGeTbrain_vote pipeline? (yes/no): " run_magetbrain
