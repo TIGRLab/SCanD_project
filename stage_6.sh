4ee3c7ff23685a3d967c72079190748ce3188ddf
GhazalehManj <126309136+GhazalehManj@users.noreply.github.com>
2025-05-15 12:24:13 -0400

    Update stage_6.sh

--- stage_6.sh
+++ stage_6.sh
@@ -1,31 +1,26 @@
 #!/bin/bash
 
 ## stage 6 (noddi_extract)
-#!/bin/bash
 
 # Function to calculate and submit array jobs
 submit_array_job() {
     local script=$1
-    local sub_size=$2
-    local n_subjects=$(( $( wc -l ./data/local/bids/participants.tsv | cut -f1 -d' ' ) - 1 ))
-    local array_job_length=$(( n_subjects / sub_size ))
-    echo "Submitting job for $script with array size: ${array_job_length}"
-    sbatch --array=0-${array_job_length} $script
+    echo "Submitting job for $script"
+    sbatch $script
 }
 
 # Function to prompt user and run selected pipeline
 run_pipeline() {
     local pipeline_name=$1
     local script_path=$2
-    local sub_size=$3
     read -p "Do you want to run the $pipeline_name pipeline? (yes/no): " run_pipeline
     if [[ "$run_pipeline" =~ ^(yes|y)$ ]]; then
         echo "Running $pipeline_name..."
-        submit_array_job $script_path $sub_size
+        submit_array_job $script_path
     else
         echo "Skipping $pipeline_name."
     fi
 }
 
-# Prompt user for each pipeline 
-run_pipeline "noddi_extract" "./code/06_extract_noddi_scinet.sh" 1
+# Prompt user for each pipeline in stage 5
+run_pipeline "noddi_extract" "./code/06_extract_noddi_scinet.sh"
