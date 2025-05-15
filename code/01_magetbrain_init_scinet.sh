0500d7fb62fa4aa89c8e31c24221bced5ede42ea
GhazalehManj <126309136+GhazalehManj@users.noreply.github.com>
2025-05-15 11:48:13 -0400

    Update 01_magetbrain_init_scinet.sh

--- code/01_magetbrain_init_scinet.sh
+++ code/01_magetbrain_init_scinet.sh
@@ -175,12 +175,12 @@ cp -r /scratch/arisvoin/shared/templateflow/atlases  "$INPUT_DIR/"
 
 ## nipoppy trackers 
 
-cd ${BASEDIR}/Neurobagel
+cd ${PROJECT_DIR}/Neurobagel
 
 source ../nipoppy/bin/activate
 
 mkdir -p derivatives/magetbraininit/0.1.0/output/
 
-ln -s ${BASEDIR}/data/local/derivatives/MAGeTbrain/magetbrain_data/*  derivatives/magetbraininit/0.1.0/output/
+ln -s ${PROJECT_DIR}/data/local/derivatives/MAGeTbrain/magetbrain_data/*  derivatives/magetbraininit/0.1.0/output/
 
 nipoppy track  --pipeline magetbraininit   --pipeline-version 0.1.0 
