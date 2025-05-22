c27434f143689aaa8626bee1894a41f050d53d1c
GhazalehManj <126309136+GhazalehManj@users.noreply.github.com>
2025-05-22 12:41:48 -0400

    Update 00_setup_data_directories.sh

--- code/00_setup_data_directories.sh
+++ code/00_setup_data_directories.sh
@@ -41,65 +41,11 @@ cp -r ${CONTAINER_DIR}/tbss_2023-10-10.simg containers/tbss_2023-10-10.simg
 
 cp -r ${CONTAINER_DIR}/magetbrain.sif containers/magetbrain.sif
 
-## edit dataset_description and bold.json files in bids
-echo '{ "Name": "ScanD", "BIDSVersion": "1.0.2" }' > data/local/bids/dataset_description.json
-echo 'participant_id' > data/local/bids/participants.tsv
 
-# Check if any *bold.json files exist
-if ls data/local/bids/*bold.json 1> /dev/null 2>&1; then
-    # Loop through each file
-    for file in data/local/bids/*bold.json; do
-        # Check if "TotalReadoutTime" is not already in the file
-        if ! grep -q "TotalReadoutTime" "$file"; then
-            # Add the "TotalReadoutTime" before the last closing brace
-            sed -i'' '$ s/}/     "TotalReadoutTime": 0.05\n}/' "$file"
-            
-            # Add a comma to the second-to-last line if necessary
-            awk 'NR==FNR { count++; next } FNR==count-2 && $0 !~ /,$/ { print $0 ","; next }1' "$file" "$file" > temp.json
-            mv -f temp.json "$file"
-        fi
-    done
-else
-    echo "No *bold.json files found in data/local/bids/"
-fi
-
-
-## copy in Erin's freesurfer licence
+## copy freesurfer licence
 cp /scratch/a/arisvoin/arisvoin/mlepage/fs_license/license.txt templates/.freesurfer.txt
 
 
-## copy in Erin's templates
+## copy templates
 echo "copying templates..this might take a bit"
 scp -r /scratch/a/arisvoin/arisvoin/mlepage/templateflow templates/.cache/
-
-cd ${CURRENT_DIR}
-
-
-
-## nipoppy tracker init
-
-module load NiaEnv/2019b python/3.11.5
-python3 -m venv nipoppy
-source nipoppy/bin/activate 
-pip install git+https://github.com/nipoppy/nipoppy.git@dev-catalog
-
-mkdir ${CURRENT_DIR}/Neurobagel/
-nipoppy init --bids-source data/local/bids/  Neurobagel/
-
-rm -rf ${CURRENT_DIR}/Neurobagel/pipelines/processing/*
-
-# Find the first sub-* folder
-first_subject=$(find "${CURRENT_DIR}/data/local/bids" -maxdepth 1 -type d -name "sub-*" | head -n 1)
-
-if [ -d "$first_subject" ]; then
-    # Check if there's any ses-* folder inside the first subject
-    if compgen -G "$first_subject/ses-*" > /dev/null; then
-        echo "Found ses-* folder in $first_subject. Copying from nipoppy..."
-        cp -r /scratch/a/arisvoin/arisvoin/mlepage/nipoppy/* "${CURRENT_DIR}/Neurobagel/pipelines/processing"
-    else
-        echo "No ses-* folder in $first_subject. Copying from nipoppy_no_session..."
-        cp -r /scratch/a/arisvoin/arisvoin/mlepage/nipoppy_no_session/* ${CURRENT_DIR}/Neurobagel/pipelines/processing
-    fi
-else
-    echo "No sub-* folder found in $BASE_DIR."
-fi
