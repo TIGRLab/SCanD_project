e2ac478d5ca9d22fbcb7196dd85892c0e5e0b443
GhazalehManj <126309136+GhazalehManj@users.noreply.github.com>
2025-06-10 17:01:50 -0400

    Update 02_qsiprep_scinet.sh

--- code/02_qsiprep_scinet.sh
+++ code/02_qsiprep_scinet.sh
@@ -114,15 +114,24 @@ singularity run --cleanenv \
 
 ## nipoppy trackers 
 
-cd ${BASEDIR}/Neurobagel
+singularity exec \
+  --bind ${SCRATCH}:${SCRATCH} \
+  --env SUBJECTS="$SUBJECTS" \
+  containers/nipoppy.sif /bin/bash -c '
+    set -euo pipefail
 
-source ../nipoppy/bin/activate
+    BASEDIR="$SCRATCH/SCanD_project"
+    cd "$BASEDIR/Neurobagel"
+    
+    mkdir -p derivatives/qsiprep/0.22.0/output/
+    ls -al derivatives/qsiprep/0.22.0/output/
 
-mkdir -p derivatives/qsiprep/0.22.0/output/
-ls -al derivatives/qsiprep/0.22.0/output/
+    ln -s "$BASEDIR/data/local/derivatives/qsiprep/0.22.0/qsiprep/"* derivatives/qsiprep/0.22.0/output/ || true
 
-ln -s ${BASEDIR}/data/local/derivatives/qsiprep/0.22.0/qsiprep/*  derivatives/qsiprep/0.22.0/output/
-
-for subject in $SUBJECTS; do
-	nipoppy track  --pipeline qsiprep   --pipeline-version 0.22.0 --participant-id sub-$subject
-done
+    for subject in $SUBJECTS; do
+      nipoppy track \
+        --pipeline qsiprep \
+        --pipeline-version 0.22.0 \
+        --participant-id sub-$subject
+    done
+  '
