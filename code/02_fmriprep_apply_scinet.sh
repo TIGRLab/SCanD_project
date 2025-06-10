d7105b68ced7850813a897229388ce431e04b20f
GhazalehManj <126309136+GhazalehManj@users.noreply.github.com>
2025-06-10 16:50:27 -0400

    Update 02_fmriprep_apply_scinet.sh

--- code/02_fmriprep_apply_scinet.sh
+++ code/02_fmriprep_apply_scinet.sh
@@ -87,15 +87,24 @@ singularity run --cleanenv \
 
 ## nipoppy trackers 
 
-cd ${BASEDIR}/Neurobagel
+singularity exec \
+  --bind ${SCRATCH}:${SCRATCH} \
+  --env SUBJECTS="$SUBJECTS" \
+  containers/nipoppy.sif /bin/bash -c '
+    set -euo pipefail
 
-source ../nipoppy/bin/activate
+    BASEDIR="$SCRATCH/SCanD_project"
+    cd "$BASEDIR/Neurobagel"
+    
+    mkdir -p derivatives/fmriprepapply/23.2.3/output/
+    ls -al derivatives/fmriprepapply/23.2.3/output/
 
-mkdir -p derivatives/fmriprepapply/23.2.3/output/
-ls -al derivatives/fmriprepapply/23.2.3/output/
+    ln -s "$BASEDIR/data/local/derivatives/fmriprep/23.2.3/"* derivatives/fmriprepapply/23.2.3/output/ || true
 
-ln -s ${BASEDIR}/data/local/derivatives/fmriprep/23.2.3/*  derivatives/fmriprepapply/23.2.3/output/
-
-for subject in $SUBJECTS; do
-	nipoppy track  --pipeline fmriprepapply   --pipeline-version 23.2.3 --participant-id sub-$subject
-done
+    for subject in $SUBJECTS; do
+      nipoppy track \
+        --pipeline fmriprepapply \
+        --pipeline-version 23.2.3 \
+        --participant-id sub-$subject
+    done
+  '
