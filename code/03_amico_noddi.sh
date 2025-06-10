77cae7e5cbc65859d9e704e22e22de84112664d7
GhazalehManj <126309136+GhazalehManj@users.noreply.github.com>
2025-06-10 17:04:43 -0400

    Update 03_amico_noddi.sh

--- code/03_amico_noddi.sh
+++ code/03_amico_noddi.sh
@@ -79,15 +79,24 @@ singularity run --cleanenv \
 
 ## nipoppy trackers 
 
-cd ${BASEDIR}/Neurobagel
+singularity exec \
+  --bind ${SCRATCH}:${SCRATCH} \
+  --env SUBJECTS="$SUBJECTS" \
+  containers/nipoppy.sif /bin/bash -c '
+    set -euo pipefail
 
-source ../nipoppy/bin/activate
+    BASEDIR="$SCRATCH/SCanD_project"
+    cd "$BASEDIR/Neurobagel"
+    
+    mkdir -p derivatives/amiconoddi/0.22.0/output/
+    ls -al derivatives/amiconoddi/0.22.0/output/
 
-mkdir -p derivatives/amiconoddi/0.22.0/output/
-ls -al derivatives/amiconoddi/0.22.0/output/
+    ln -s "$BASEDIR/data/local/derivatives/qsiprep/0.22.0/amico_noddi/"* derivatives/amiconoddi/0.22.0/output/ || true
 
-ln -s ${BASEDIR}/data/local/derivatives/qsiprep/0.22.0/amico_noddi/*  derivatives/amiconoddi/0.22.0/output/
-
-for subject in $SUBJECTS; do
-	nipoppy track  --pipeline amiconoddi   --pipeline-version 0.22.0 --participant-id sub-$subject
-done
+    for subject in $SUBJECTS; do
+      nipoppy track \
+        --pipeline amiconoddi \
+        --pipeline-version 0.22.0 \
+        --participant-id sub-$subject
+    done
+  '
