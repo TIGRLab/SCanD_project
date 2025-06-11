c029b7f9c0fb5a95f7ea58bbecb03b94cbe9dcff
GhazalehManj <126309136+GhazalehManj@users.noreply.github.com>
2025-06-11 16:28:39 -0400

    Update 02_ciftify_anat_scinet.sh

--- code/02_ciftify_anat_scinet.sh
+++ code/02_ciftify_anat_scinet.sh
@@ -38,26 +38,18 @@ export LOGS_DIR=${BASEDIR}/logs
 mkdir -vp ${OUTPUT_DIR} 
 
 
-## get the subject list from a combo of the array id, the participants.tsv and the chunk 
-bigger_bit=`echo "($SLURM_ARRAY_TASK_ID + 1) * ${SUB_SIZE}" | bc`
+SUBJECT_FOLDERS=(${SUBJECTS_DIR}/*long*)
+SUBJECTS=("${SUBJECT_FOLDERS[@]##*/}")  # Just the folder names, like sub-CMH00000001_long-01
 
-N_SUBJECTS=$(( $( wc -l ${BIDS_DIR}/participants.tsv | cut -f1 -d' ' ) - 1 ))
-array_job_length=$(echo "$N_SUBJECTS/${SUB_SIZE}" | bc)
-Tail=$((N_SUBJECTS-(array_job_length*SUB_SIZE)))
-
-if [ "$SLURM_ARRAY_TASK_ID" -eq "$array_job_length" ]; then
-    SUBJECTS=`sed -n -E "s/sub-(\S*)\>.*/\1/gp" ${BIDS_DIR}/participants.tsv  | head -n ${N_SUBJECTS} | tail -n ${Tail}`
-else
-    SUBJECTS=`sed -n -E "s/sub-(\S*)\>.*/\1/gp" ${BIDS_DIR}/participants.tsv | head -n ${bigger_bit} | tail -n ${SUB_SIZE}`
-fi
-
+SUBJECT_INDEX=${SLURM_ARRAY_TASK_ID}
+SELECTED_SUBJECT=${SUBJECTS[$SUBJECT_INDEX]}
  
 singularity exec --cleanenv \
     -B ${SUBJECTS_DIR}:/freesurfer \
     -B ${OUTPUT_DIR}:/out \
     -B ${ORIG_FS_LICENSE}:/li \
     ${SING_CONTAINER} \
-    ciftify_recon_all --fs-subjects-dir /freesurfer sub-${SUBJECTS} --ciftify-work-dir /out --fs-license /li
+    ciftify_recon_all --fs-subjects-dir /freesurfer ${SELECTED_SUBJECT} --ciftify-work-dir /out --fs-license /li
 
 # Capture the exit status of the first command
 exitcode=$?
