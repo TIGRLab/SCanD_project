272e5ac909eed0996b4040833608354955de726e
GhazalehManj <126309136+GhazalehManj@users.noreply.github.com>
2025-07-04 16:12:12 -0400

    Update 06_extract_to_share_terminal.sh

--- code/06_extract_to_share_terminal.sh
+++ code/06_extract_to_share_terminal.sh
@@ -7,16 +7,16 @@ PROJECT_DIR=$(dirname "${SCRIPT_DIR}")
 module load apptainer/1.3.5
 
 # sharing magetbrain outputs
-mkdir -p ${PROJECT_DIR}/data/local/MAGeTbrain/magetbrain_data/QC
-singularity exec --cleanenv -B ${PROJECT_DIR}/data/local/MAGeTbrain/magetbrain_data:/data ${PROJECT_DIR}/containers/magetbrain.sif /bin/bash -c "export LANG=C.UTF-8 && export LC_ALL=C.UTF-8 && export LD_LIBRARY_PATH=/opt/minc/1.9.18/lib:\$LD_LIBRARY_PATH && collect_volumes.sh /data/output/fusion/majority_vote/*.mnc > /data/QC/volumes.csv"
+mkdir -p ${PROJECT_DIR}/data/local/derivatives/MAGeTbrain/magetbrain_data/QC
+singularity exec --cleanenv -B ${PROJECT_DIR}/data/local/derivatives/MAGeTbrain/magetbrain_data:/data ${PROJECT_DIR}/containers/magetbrain.sif /bin/bash -c "export LANG=C.UTF-8 && export LC_ALL=C.UTF-8 && export LD_LIBRARY_PATH=/opt/minc/1.9.18/lib:\$LD_LIBRARY_PATH && collect_volumes.sh /data/output/fusion/majority_vote/*.mnc > /data/QC/volumes.csv"
 source ${PROJECT_DIR}/code/magetbrain_QC.sh
 
 mkdir -p ${PROJECT_DIR}/data/share/magetbrain/input
 mkdir -p ${PROJECT_DIR}/data/share/magetbrain/fusion
 
-rsync -a ${PROJECT_DIR}/data/local/MAGeTbrain/magetbrain_data/output/fusion/majority_vote/*labels.mnc ${PROJECT_DIR}/data/share/magetbrain/fusion
-rsync -a ${PROJECT_DIR}/data/local/MAGeTbrain/magetbrain_data/input/subjects/brains/*.mnc* ${PROJECT_DIR}/data/share/magetbrain/input
-rsync -a ${PROJECT_DIR}/data/local/MAGeTbrain/magetbrain_data/QC ${PROJECT_DIR}/data/share/magetbrain/
+rsync -a ${PROJECT_DIR}/data/local/derivatives/MAGeTbrain/magetbrain_data/output/fusion/majority_vote/*labels.mnc ${PROJECT_DIR}/data/share/magetbrain/fusion
+rsync -a ${PROJECT_DIR}/data/local/derivatives/MAGeTbrain/magetbrain_data/input/subjects/brains/*.mnc* ${PROJECT_DIR}/data/share/magetbrain/input
+rsync -a ${PROJECT_DIR}/data/local/derivatives/MAGeTbrain/magetbrain_data/QC ${PROJECT_DIR}/data/share/magetbrain/
 
 
 ## Generate qsiprep motion metrics and extract NODDI indices
