1d54e796379aae711b2f76f1bdc173bdffe86e72
GhazalehManj <126309136+GhazalehManj@users.noreply.github.com>
2025-07-04 16:11:16 -0400

    Update 06_extract_to_share_terminal.sh

--- code/06_extract_to_share_terminal.sh
+++ code/06_extract_to_share_terminal.sh
@@ -24,13 +24,13 @@ mkdir -p ${PROJECT_DIR}/data/share/qsiprep/0.22.0/
 rsync -a ${PROJECT_DIR}/data/local/derivatives/qsiprep/0.22.0/qsiprep/qsiprep_metrics.csv ${PROJECT_DIR}/data/share/qsiprep/0.22.0/
 
 # sharing magetbrain outputs
-mkdir -p ${PROJECT_DIR}/data/local/MAGeTbrain/magetbrain_data/QC
-singularity exec --cleanenv -B ${PROJECT_DIR}/data/local/MAGeTbrain/magetbrain_data:/data ${PROJECT_DIR}/containers/magetbrain.sif /bin/bash -c "export LANG=C.UTF-8 && export LC_ALL=C.UTF-8 && export LD_LIBRARY_PATH=/opt/minc/1.9.18/lib:\$LD_LIBRARY_PATH && collect_volumes.sh /data/output/fusion/majority_vote/*.mnc > /data/QC/volumes.csv"
+mkdir -p ${PROJECT_DIR}/data/local/derivatives/MAGeTbrain/magetbrain_data/QC
+singularity exec --cleanenv -B ${PROJECT_DIR}/data/local/derivatives/MAGeTbrain/magetbrain_data:/data ${PROJECT_DIR}/containers/magetbrain.sif /bin/bash -c "export LANG=C.UTF-8 && export LC_ALL=C.UTF-8 && export LD_LIBRARY_PATH=/opt/minc/1.9.18/lib:\$LD_LIBRARY_PATH && collect_volumes.sh /data/output/fusion/majority_vote/*.mnc > /data/QC/volumes.csv"
 source ${PROJECT_DIR}/code/magetbrain_QC.sh
 
 mkdir -p ${PROJECT_DIR}/data/share/magetbrain/input
 mkdir -p ${PROJECT_DIR}/data/share/magetbrain/fusion
 
-rsync -a ${PROJECT_DIR}/data/local/MAGeTbrain/magetbrain_data/output/fusion/majority_vote/*labels.mnc ${PROJECT_DIR}/data/share/magetbrain/fusion
-rsync -a ${PROJECT_DIR}/data/local/MAGeTbrain/magetbrain_data/input/subjects/brains/*.mnc* ${PROJECT_DIR}/data/share/magetbrain/input
-rsync -a ${PROJECT_DIR}/data/local/MAGeTbrain/magetbrain_data/QC ${PROJECT_DIR}/data/share/magetbrain/
+rsync -a ${PROJECT_DIR}/data/local/derivatives/MAGeTbrain/magetbrain_data/output/fusion/majority_vote/*labels.mnc ${PROJECT_DIR}/data/share/magetbrain/fusion
+rsync -a ${PROJECT_DIR}/data/local/derivatives/MAGeTbrain/magetbrain_data/input/subjects/brains/*.mnc* ${PROJECT_DIR}/data/share/magetbrain/input
+rsync -a ${PROJECT_DIR}/data/local/derivatives/MAGeTbrain/magetbrain_data/QC ${PROJECT_DIR}/data/share/magetbrain/
