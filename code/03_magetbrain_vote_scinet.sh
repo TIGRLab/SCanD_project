69002b7a3a08a4c437aa6ad10a0c7331d6a09433
GhazalehManj <126309136+GhazalehManj@users.noreply.github.com>
2025-07-10 13:11:10 -0400

    Update 03_magetbrain_vote_scinet.sh

--- code/03_magetbrain_vote_scinet.sh
+++ code/03_magetbrain_vote_scinet.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=magetbrain_vote
 #SBATCH --output=logs/%x_%j.out
 #SBATCH --nodes=1
-#SBATCH --cpus-per-task=16
+#SBATCH --cpus-per-task=4
 #SBATCH --time=10:00:00
 #SBATCH --mem-per-cpu=10000
 
