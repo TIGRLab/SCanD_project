6db89026f2700c6cc2fe30eec70842b9adb7a4e5
GhazalehManj <126309136+GhazalehManj@users.noreply.github.com>
2025-07-10 20:10:16 -0400

    Update 01_mriqc_scinet.sh

--- code/01_mriqc_scinet.sh
+++ code/01_mriqc_scinet.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=mriqc
 #SBATCH --output=logs/%x_%j.out 
 #SBATCH --nodes=1
-#SBATCH --cpus-per-task=10
+#SBATCH --cpus-per-task=8
 #SBATCH --time=08:00:00
 #SBATCH --mem-per-cpu=4000
 
