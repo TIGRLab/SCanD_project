58483b9fe7b7e68cb174a2386d7cbaf09a5daf83
GhazalehManj <126309136+GhazalehManj@users.noreply.github.com>
2025-07-12 17:38:03 -0400

    Update 03_magetbrain_vote_scinet.sh

--- code/03_magetbrain_vote_scinet.sh
+++ code/03_magetbrain_vote_scinet.sh
@@ -3,7 +3,7 @@
 #SBATCH --output=logs/%x_%j.out
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=8
-#SBATCH --time=16:00:00
+#SBATCH --time=20:00:00
 #SBATCH --mem-per-cpu=4000
 
 module load apptainer/1.3.5
