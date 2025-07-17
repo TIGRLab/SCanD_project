69d51f056625d22d925496f3baeef145b60c502b
GhazalehManj <126309136+GhazalehManj@users.noreply.github.com>
2025-07-17 11:21:17 -0400

    Update 01_smriprep_scinet.sh

--- code/01_smriprep_scinet.sh
+++ code/01_smriprep_scinet.sh
@@ -77,7 +77,7 @@ singularity exec --cleanenv \
 singularity exec \
   --env BASEDIR="$BASEDIR" \
   --env SUBJECTS="$SUBJECTS" \
-  containers/nipoppy.sif /bin/bash -c '
+  ${BASEDIR}/containers/nipoppy.sif /bin/bash -c '
     set -euo pipefail
 
     cd "$BASEDIR/Neurobagel"
