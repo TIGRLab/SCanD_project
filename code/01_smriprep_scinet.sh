cebc67628e5c67c96117eef8e80c23f7cb6da7db
GhazalehManj <126309136+GhazalehManj@users.noreply.github.com>
2025-07-17 11:09:19 -0400

    Update 01_smriprep_scinet.sh

--- code/01_smriprep_scinet.sh
+++ code/01_smriprep_scinet.sh
@@ -75,12 +75,11 @@ singularity exec --cleanenv \
 ## nipoppy trackers 
 
 singularity exec \
-  --bind ${SCRATCH}:${SCRATCH} \
+  --env BASEDIR="$BASEDIR" \
   --env SUBJECTS="$SUBJECTS" \
   containers/nipoppy.sif /bin/bash -c '
     set -euo pipefail
 
-    BASEDIR="$SCRATCH/SCanD_project"
     cd "$BASEDIR/Neurobagel"
     
     mkdir -p derivatives/smriprep/23.2.3/output/
