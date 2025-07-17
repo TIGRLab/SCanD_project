f1e05f789de51840e36766715c552dbbd8460603
GhazalehManj <126309136+GhazalehManj@users.noreply.github.com>
2025-07-17 11:20:10 -0400

    Update 01_freesurfer_long_scinet.sh

--- code/01_freesurfer_long_scinet.sh
+++ code/01_freesurfer_long_scinet.sh
@@ -96,7 +96,7 @@ fi
 singularity exec \
   --env BASEDIR="$BASEDIR" \
   --env SUBJECTS="$SUBJECTS" \
-  containers/nipoppy.sif /bin/bash -c '
+  ${BASEDIR}/containers/nipoppy.sif /bin/bash -c '
     set -euo pipefail
 
     cd "$BASEDIR/Neurobagel"
