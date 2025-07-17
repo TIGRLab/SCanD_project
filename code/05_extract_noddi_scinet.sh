5df85c7ec37a5a41ccfce9744397922f3b4ae399
GhazalehManj <126309136+GhazalehManj@users.noreply.github.com>
2025-07-17 11:19:18 -0400

    Update 05_extract_noddi_scinet.sh

--- code/05_extract_noddi_scinet.sh
+++ code/05_extract_noddi_scinet.sh
@@ -42,12 +42,11 @@ done
 ## nipoppy trackers 
 
 singularity exec \
-  --bind ${SCRATCH}:${SCRATCH} \
+  --env BASEDIR="$BASEDIR" \
   --env SUBJECTS="$SUBJECTS" \
   containers/nipoppy.sif /bin/bash -c '
     set -euo pipefail
 
-    BASEDIR="$SCRATCH/SCanD_project"
     cd "$BASEDIR/Neurobagel"
     
     mkdir -p derivatives/extractnoddi/0.1.1/output/
