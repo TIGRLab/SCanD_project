1ed084e6388a438f866dff170606c25b200ac1d9
GhazalehManj <126309136+GhazalehManj@users.noreply.github.com>
2025-07-17 11:58:23 -0400

    Update 06_extract_to_share_terminal.sh

--- code/06_extract_to_share_terminal.sh
+++ code/06_extract_to_share_terminal.sh
@@ -1,37 +1,35 @@
-# A script to extract the bits that we want to share back with the corsotium
-
-
+#!/bin/bash
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-PROJECT_DIR=$(dirname "${SCRIPT_DIR}")
+BASEDIR=${SCRIPT_DIR}/..
 
 module load apptainer/1.3.5
 
 # sharing magetbrain outputs
-mkdir -p ${PROJECT_DIR}/data/local/derivatives/MAGeTbrain/magetbrain_data/QC
-singularity exec --cleanenv -B ${PROJECT_DIR}/data/local/derivatives/MAGeTbrain/magetbrain_data:/data ${PROJECT_DIR}/containers/magetbrain.sif /bin/bash -c "export LANG=C.UTF-8 && export LC_ALL=C.UTF-8 && export LD_LIBRARY_PATH=/opt/minc/1.9.18/lib:\$LD_LIBRARY_PATH && collect_volumes.sh /data/output/fusion/majority_vote/*.mnc > /data/QC/volumes.csv"
-source ${PROJECT_DIR}/code/magetbrain_QC.sh
+mkdir -p ${BASEDIR}/data/local/derivatives/MAGeTbrain/magetbrain_data/QC
+singularity exec --cleanenv -B ${BASEDIR}/data/local/derivatives/MAGeTbrain/magetbrain_data:/data ${BASEDIR}/containers/magetbrain.sif /bin/bash -c "export LANG=C.UTF-8 && export LC_ALL=C.UTF-8 && export LD_LIBRARY_PATH=/opt/minc/1.9.18/lib:\$LD_LIBRARY_PATH && collect_volumes.sh /data/output/fusion/majority_vote/*.mnc > /data/QC/volumes.csv"
+source ${BASEDIR}/code/magetbrain_QC.sh
 
-mkdir -p ${PROJECT_DIR}/data/share/magetbrain/input
-mkdir -p ${PROJECT_DIR}/data/share/magetbrain/fusion
+mkdir -p ${BASEDIR}/data/share/magetbrain/input
+mkdir -p ${BASEDIR}/data/share/magetbrain/fusion
 
-rsync -a ${PROJECT_DIR}/data/local/derivatives/MAGeTbrain/magetbrain_data/output/fusion/majority_vote/*labels.mnc ${PROJECT_DIR}/data/share/magetbrain/fusion
-rsync -a ${PROJECT_DIR}/data/local/derivatives/MAGeTbrain/magetbrain_data/input/subjects/brains/*.mnc* ${PROJECT_DIR}/data/share/magetbrain/input
-rsync -a ${PROJECT_DIR}/data/local/derivatives/MAGeTbrain/magetbrain_data/QC ${PROJECT_DIR}/data/share/magetbrain/
+rsync -a ${BASEDIR}/data/local/derivatives/MAGeTbrain/magetbrain_data/output/fusion/majority_vote/*labels.mnc ${BASEDIR}/data/share/magetbrain/fusion
+rsync -a ${BASEDIR}/data/local/derivatives/MAGeTbrain/magetbrain_data/input/subjects/brains/*.mnc* ${BASEDIR}/data/share/magetbrain/input
+rsync -a ${BASEDIR}/data/local/derivatives/MAGeTbrain/magetbrain_data/QC ${BASEDIR}/data/share/magetbrain/
 
 
 ## Generate qsiprep motion metrics and extract NODDI indices
 module load  python/3.10
 
 # Create a directory for virtual environments if it doesn't exist
-mkdir ${PROJECT_DIR}/../.virtualenvs
-cd ${PROJECT_DIR}/../.virtualenvs
-virtualenv --system-site-packages ${PROJECT_DIR}/../.virtualenvs/myenv
+mkdir ${BASEDIR}/../.virtualenvs
+cd ${BASEDIR}/../.virtualenvs
+virtualenv --system-site-packages ${BASEDIR}/../.virtualenvs/myenv
 
 # Activate the virtual environment
-source ${PROJECT_DIR}/../.virtualenvs/myenv/bin/activate
+source ${BASEDIR}/../.virtualenvs/myenv/bin/activate
 
-cd ${PROJECT_DIR}
-python3 ${PROJECT_DIR}/code/gen_qsiprep_motion_metrics.py
+cd ${BASEDIR}
+python3 ${BASEDIR}/code/gen_qsiprep_motion_metrics.py
 
-mkdir -p ${PROJECT_DIR}/data/share/qsiprep/0.22.0/
-rsync -a ${PROJECT_DIR}/data/local/derivatives/qsiprep/0.22.0/qsiprep/qsiprep_metrics.csv ${PROJECT_DIR}/data/share/qsiprep/0.22.0/
+mkdir -p ${BASEDIR}/data/share/qsiprep/0.22.0/
+rsync -a ${BASEDIR}/data/local/derivatives/qsiprep/0.22.0/qsiprep/qsiprep_metrics.csv ${BASEDIR}/data/share/qsiprep/0.22.0/
