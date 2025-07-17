4496441a1c0c6338aab70af7a4093adda4b3c32e
GhazalehManj <126309136+GhazalehManj@users.noreply.github.com>
2025-07-17 11:14:57 -0400

    Update 02_tractography_single_scinet.sh

--- code/02_tractography_single_scinet.sh
+++ code/02_tractography_single_scinet.sh
@@ -91,12 +91,11 @@ singularity run --cleanenv \
 ## nipoppy trackers 
 
 singularity exec \
-  --bind ${SCRATCH}:${SCRATCH} \
+  --env BASEDIR="$BASEDIR" \
   --env SUBJECTS="$SUBJECTS" \
   containers/nipoppy.sif /bin/bash -c '
     set -euo pipefail
 
-    BASEDIR="$SCRATCH/SCanD_project"
     cd "$BASEDIR/Neurobagel"
     
     mkdir -p derivatives/tractographysingle/0.22.0/output/
