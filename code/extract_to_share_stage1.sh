8306b30a77b0e0288fe00431da30abb504370e21
GhazalehManj <126309136+GhazalehManj@users.noreply.github.com>
2025-08-11 14:46:18 -0400

    Update extract_to_share_stage1.sh

--- code/extract_to_share_stage1.sh
+++ code/extract_to_share_stage1.sh
@@ -45,10 +45,9 @@ fi
 
 
 ## run the mriqc group step and copy over all outputs
-project_id=$(cat ${BASEDIR}/project_id)
 MRIQC_SHARE_DIR=${BASEDIR}/data/share/mriqc/24.0.0
 MRIQC_LOCAL_DIR=${BASEDIR}/data/local/derivatives/mriqc/24.0.0
-export WORK_DIR=${BBUFFER}/SCanD/${project_id}/mriqc
+export WORK_DIR=${SLURM_TMPDIR}/SCanD/mriqc
 mkdir -vp ${WORK_DIR}
 
 if [ -d "$MRIQC_LOCAL_DIR" ];
