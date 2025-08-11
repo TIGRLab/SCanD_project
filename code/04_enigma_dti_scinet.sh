d58aa97e9878c8500bdce42836e1a1a2afe6e6da
GhazalehManj <126309136+GhazalehManj@users.noreply.github.com>
2025-08-11 10:14:40 -0400

    Update 04_enigma_dti_scinet.sh

--- code/04_enigma_dti_scinet.sh
+++ code/04_enigma_dti_scinet.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=enigma_dti
 #SBATCH --output=logs/%x_%j.out
 #SBATCH --nodes=1
-#SBATCH --cpus-per-task=40
+#SBATCH --cpus-per-task=192
 #SBATCH --time=01:00:00
 
 # Load necessary modules if needed
