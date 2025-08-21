da3d895b7a4603d11b7412a8e0fe9834701ac067
GhazalehManj <126309136+GhazalehManj@users.noreply.github.com>
2025-08-20 23:29:51 -0400

    Update 04_enigma_dti_scinet.sh

--- code/04_enigma_dti_scinet.sh
+++ code/04_enigma_dti_scinet.sh
@@ -64,7 +64,7 @@ singularity exec \
     mkdir -p derivatives/enigmadti/0.1.1/output/
     ls -al derivatives/enigmadti/0.1.1/output/
 
-    ln -s "$BASEDIR/data/local/data/local/enigmaDTI/" derivatives/enigmadti/0.1.1/output/ || true
+    ln -s "$BASEDIR/data/local/enigmaDTI/" derivatives/enigmadti/0.1.1/output/ || true
 
     nipoppy track  --pipeline enigmadti  --pipeline-version 0.1.1 
   '
