8be2a1b050fe0c16eb358c5c3300a0631b1aa3e4
GhazalehManj <126309136+GhazalehManj@users.noreply.github.com>
2025-08-23 14:48:55 -0400

    Update 01_mriqc_scinet.sh

--- code/01_mriqc_scinet.sh
+++ code/01_mriqc_scinet.sh
@@ -2,9 +2,9 @@
 #SBATCH --job-name=mriqc
 #SBATCH --output=logs/%x_%j.out 
 #SBATCH --nodes=1
-#SBATCH --cpus-per-task=8
+#SBATCH --cpus-per-task=16
 #SBATCH --time=08:00:00
-#SBATCH --mem-per-cpu=4000
+#SBATCH --mem-per-cpu=8000
 
 SUB_SIZE=1 ## number of subjects to run
 export THREADS_PER_COMMAND=2
