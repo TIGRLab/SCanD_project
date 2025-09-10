80cb917210688e6a1d6b489a5aa4a5394fb50975
GhazalehManj <126309136+GhazalehManj@users.noreply.github.com>
2025-09-10 15:41:46 -0400

    Update 06_extract_to_share_slurm.sh

--- code/06_extract_to_share_slurm.sh
+++ code/06_extract_to_share_slurm.sh
@@ -291,7 +291,7 @@ fi
 
 
 
-TRACTIFY_MULTI_LOCAL_DIR=${BASEDIR}/data/local/derivatives/qsiprep/0.22.0/qsirecon-MRtrix3_act-HSVS
+TRACTIFY_MULTI_LOCAL_DIR=${BASEDIR}/data/local/derivatives/qsiprep/0.22.0/tractography/qsirecon-MRtrix3_act-HSVS
 TRACTIFY_SHARE_DIR=${BASEDIR}/data/share/tractify
 
 if [ -d "${TRACTIFY_MULTI_LOCAL_DIR}" ];
@@ -314,7 +314,7 @@ fi
 
 
 
-TRACTIFY_SINGLE_LOCAL_DIR=${BASEDIR}/data/local/derivatives/qsiprep/0.22.0/qsirecon-MRtrix3_fork-SS3T_act-HSVS
+TRACTIFY_SINGLE_LOCAL_DIR=${BASEDIR}/data/local/derivatives/qsiprep/0.22.0/tractography/qsirecon-MRtrix3_fork-SS3T_act-HSVS
 TRACTIFY_SHARE_DIR=${BASEDIR}/data/share/tractify
 
 if [ -d "${TRACTIFY_SINGLE_LOCAL_DIR}" ];
