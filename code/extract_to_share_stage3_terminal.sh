e6a7430ada258f27484ee5447f4135ce0ac39c51
GhazalehManj <126309136+GhazalehManj@users.noreply.github.com>
2025-10-01 19:52:33 -0400

    Update extract_to_share_stage3_terminal.sh

--- code/extract_to_share_stage3_terminal.sh
+++ code/extract_to_share_stage3_terminal.sh
@@ -7,7 +7,7 @@ BASEDIR=${SCRIPT_DIR}/..
 
 echo "Running qsiprep_metrics.csv"
 
-## Generate qsiprep motion metrics and extract NODDI indices
+## Generate qsiprep motion metrics and magetbrain
 module load python/3.6.8
 
 # Create a directory for virtual environments if it doesn't exist
