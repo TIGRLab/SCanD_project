56fa6811f113204964597bdf4f16afbe072f48a9
GhazalehManj <126309136+GhazalehManj@users.noreply.github.com>
2025-10-01 19:52:02 -0400

    Update extract_to_share_stage3_terminal.sh

--- code/extract_to_share_stage3_terminal.sh
+++ code/extract_to_share_stage3_terminal.sh
@@ -8,7 +8,7 @@ module load apptainer/1.3.5
 
 echo "Running qsiprep_metrics.csv"
 
-## Generate qsiprep motion metrics and extract NODDI indices
+## Generate qsiprep motion metrics and magetbrain
 module python/3.10.13
 
 # Create a directory for virtual environments if it doesn't exist
