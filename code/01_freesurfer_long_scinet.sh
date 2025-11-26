696083123647f55b25700a3517f8db1d20734782
GhazalehManj <126309136+GhazalehManj@users.noreply.github.com>
2025-11-26 15:41:02 -0500

    Update 01_freesurfer_long_scinet.sh

--- code/01_freesurfer_long_scinet.sh
+++ code/01_freesurfer_long_scinet.sh
@@ -86,7 +86,7 @@ singularity run --cleanenv \
 #   --skull-strip-t1w force \ # uncomment this line if skull stripping has aleady been done
 
 export SUBJECTS_DIR=${BASEDIR}/data/local/derivatives/freesurfer/7.4.1
-SUBJECT_LONG_DIRS=$(find $SUBJECTS_DIR -maxdepth 1 -name "${SUBJECTS}*.long.${SUBJECTS}" -type d)
+SUBJECT_LONG_DIRS=$(find $SUBJECTS_DIR -maxdepth 1 -name ""sub-${SUBJECTS}*.long.sub-${SUBJECTS}" " -type d)
 
 if [[ -z "$SUBJECT_LONG_DIRS" ]]; then
     # No longitudinal dirs → use notlong tracker_config
