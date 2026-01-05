806b1a8c6a91ed9be8a1acc422e8217338191fb8
GhazalehManj <126309136+GhazalehManj@users.noreply.github.com>
2026-01-04 19:48:16 -0500

    Update 03_run_noddi_reg_scinet.sh

--- code/03_run_noddi_reg_scinet.sh
+++ code/03_run_noddi_reg_scinet.sh
@@ -184,14 +184,14 @@ SUBJECT_LONG_DIRS=$(find $SUBJECTS_DIR -maxdepth 1 -name ""sub-${SUBJECTS}*.long
 
 if [[ -z "$SUBJECT_LONG_DIRS" ]]; then
     # No longitudinal dirs → use notlong tracker_config
-    rm -rf Neurobagel/pipelines/processing/noddireg-0.22.0/tracker_config.json
+    rm -rf ${BASEDIR}/Neurobagel/pipelines/processing/noddireg-0.22.0/tracker_config.json
     cp -r /scratch/arisvoin/shared/noddireg_notlong/tracker_config.json \
-          Neurobagel/pipelines/processing/noddireg-0.22.0/
+          ${BASEDIR}/Neurobagel/pipelines/processing/noddireg-0.22.0/
 else
     # Longitudinal dirs found → use long tracker_config
-    rm -rf Neurobagel/pipelines/processing/noddireg-0.22.0/tracker_config.json
+    rm -rf ${BASEDIR}/Neurobagel/pipelines/processing/noddireg-0.22.0/tracker_config.json
     cp -r /scratch/arisvoin/shared/nipoppy/noddireg-0.22.0/tracker_config.json \
-          Neurobagel/pipelines/processing/noddireg-0.22.0/
+          ${BASEDIR}/Neurobagel/pipelines/processing/noddireg-0.22.0/
 fi
 
 
