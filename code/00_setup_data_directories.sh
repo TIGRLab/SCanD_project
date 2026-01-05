fd5734ea231c1e89f7d68d8cd59cfbbb13e98314
GhazalehManj <126309136+GhazalehManj@users.noreply.github.com>
2026-01-04 19:33:08 -0500

    Update 00_setup_data_directories.sh

--- code/00_setup_data_directories.sh
+++ code/00_setup_data_directories.sh
@@ -35,6 +35,8 @@ cp -r ${CONTAINER_DIR}/tbss_2023-10-10.simg containers/tbss_2023-10-10.simg
 
 cp -r ${CONTAINER_DIR}/magetbrain.sif containers/magetbrain.sif
 
+ln -s ${CONTAINER_DIR}/noddi_postproc-v.1.0.simg containers/noddi_postproc-v.1.0.simg
+
 ln -s ${CONTAINER_DIR}/nipoppy.sif  containers/nipoppy.sif
 
 
