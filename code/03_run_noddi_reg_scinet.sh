6b42faa2217960ce6be6534dd1a990362392f432
GhazalehManj <126309136+GhazalehManj@users.noreply.github.com>
2026-01-04 19:44:58 -0500

    Update 03_run_noddi_reg_scinet.sh

--- code/03_run_noddi_reg_scinet.sh
+++ code/03_run_noddi_reg_scinet.sh
@@ -176,6 +176,21 @@ for SUBJECT in ${SUBJECTS}; do
   done
 done
 
+export SUBJECTS_DIR=${BASEDIR}/data/local/derivatives/freesurfer/7.4.1
+SUBJECT_LONG_DIRS=$(find $SUBJECTS_DIR -maxdepth 1 -name ""sub-${SUBJECTS}*.long.sub-${SUBJECTS}" " -type d)
+
+if [[ -z "$SUBJECT_LONG_DIRS" ]]; then
+    # No longitudinal dirs → use notlong tracker_config
+    rm -rf Neurobagel/pipelines/processing/noddireg-0.22.0/tracker_config.json
+    cp -r /scratch/arisvoin/shared/noddireg_notlong/tracker_config.json \
+          Neurobagel/pipelines/processing/noddireg-0.22.0/
+else
+    # Longitudinal dirs found → use long tracker_config
+    rm -rf Neurobagel/pipelines/processing/noddireg-0.22.0/tracker_config.json
+    cp -r /scratch/arisvoin/shared/nipoppy/noddireg-0.22.0/tracker_config.json \
+          Neurobagel/pipelines/processing/noddireg-0.22.0/
+fi
+
 
 ## nipoppy trackers
 
