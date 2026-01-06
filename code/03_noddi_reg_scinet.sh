8570968461f48644522d8f362247d5569cc5a0d0
GhazalehManj <126309136+GhazalehManj@users.noreply.github.com>
2026-01-06 14:06:07 -0500

    Update 03_noddi_reg_scinet.sh

--- code/03_noddi_reg_scinet.sh
+++ code/03_noddi_reg_scinet.sh
@@ -175,7 +175,7 @@ done
 
 
 export SUBJECTS_DIR=${BASEDIR}/data/local/derivatives/freesurfer/7.4.1
-SUBJECT_LONG_DIRS=$(find $SUBJECTS_DIR -maxdepth 1 -name ""sub-${SUBJECTS}*.long.sub-${SUBJECTS}" " -type d)
+SUBJECT_LONG_DIRS=$(find "$SUBJECTS_DIR" -maxdepth 1 -type d -name "*.long.*" | head -n 1)
 
 if [[ -z "$SUBJECT_LONG_DIRS" ]]; then
     # No longitudinal dirs → use notlong tracker_config
