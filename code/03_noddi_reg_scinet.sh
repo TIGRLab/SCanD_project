32f8767dac3755a7cc3033dfb0bdecdb0be682b8
GhazalehManj <126309136+GhazalehManj@users.noreply.github.com>
2026-01-06 15:35:02 -0500

    Update 03_noddi_reg_scinet.sh

--- code/03_noddi_reg_scinet.sh
+++ code/03_noddi_reg_scinet.sh
@@ -15,7 +15,7 @@ BASEDIR=${SLURM_SUBMIT_DIR}
 # PATHS
 # =========================
 export BIDS_DIR=${BASEDIR}/data/local/bids
-export SUBJECTS_DIR=${BASEDIR}/data/local/derivatives/freesurfer/7.4.1
+export SUBJECTS_DIR=${BASEDIR}/data/local/derivatives/fmriprep/23.2.3/sourcedata/freesurfer
 export QSIPREP_DIR=${BASEDIR}/data/local/derivatives/qsiprep/0.22.0/qsiprep
 export NODDI_DIR=${BASEDIR}/data/local/derivatives/qsiprep/0.22.0/amico_noddi/qsirecon-NODDI
 export CIFTIFY_DIR=${BASEDIR}/data/local/derivatives/ciftify/ciftify_noddi_reg
