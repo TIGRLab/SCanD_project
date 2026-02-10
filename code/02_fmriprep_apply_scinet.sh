d890024bba84d7e340493f12af009e1ff97a5df5
GhazalehManj <126309136+GhazalehManj@users.noreply.github.com>
2026-02-10 16:26:49 -0500

    Update 02_fmriprep_apply_scinet.sh

--- code/02_fmriprep_apply_scinet.sh
+++ code/02_fmriprep_apply_scinet.sh
@@ -34,10 +34,10 @@ export BIDS_DIR=${BASEDIR}/data/local/bids
 ## these folders envs need to be set up for this script to run properly 
 ## see notebooks/00_setting_up_envs.md for the set up instructions
 export FMRIPREP_HOME=${BASEDIR}/templates
-export SING_CONTAINER=${BASEDIR}/containers/fmriprep-23.2.3.simg
+export SING_CONTAINER=${BASEDIR}/containers/fmriprep-25.2.4.simg
 
 ## setting up the output folders
-export OUTPUT_DIR=${BASEDIR}/data/local/derivatives/fmriprep/23.2.3  # use if version of fmriprep >=20.2
+export OUTPUT_DIR=${BASEDIR}/data/local/derivatives/fmriprep/25.2.4  # use if version of fmriprep >=20.2
 #export OUTPUT_DIR=${BASEDIR}/data/local/ # use if version of fmriprep <=21.0
 
 export WORK_DIR=${SLURM_TMPDIR}/SCanD/fmriprep
@@ -97,15 +97,15 @@ singularity exec \
     BASEDIR="$SCRATCH/SCanD_project"
     cd "${ROOT_DIR}/Neurobagel"
     
-    mkdir -p derivatives/fmriprepapply/23.2.3/output/
-    ls -al derivatives/fmriprepapply/23.2.3/output/
+    mkdir -p derivatives/fmriprepapply/25.2.4/output/
+    ls -al derivatives/fmriprepapply/25.2.4/output/
 
-    ln -s "${ROOT_DIR}/data/local/derivatives/fmriprep/23.2.3/"* derivatives/fmriprepapply/23.2.3/output/ || true
+    ln -s "${ROOT_DIR}/data/local/derivatives/fmriprep/25.2.4/"* derivatives/fmriprepapply/25.2.4/output/ || true
 
     for subject in $SUBJECTS; do
       nipoppy track \
         --pipeline fmriprepapply \
-        --pipeline-version 23.2.3 \
+        --pipeline-version 25.2.4 \
         --participant-id sub-$subject
     done
   '
