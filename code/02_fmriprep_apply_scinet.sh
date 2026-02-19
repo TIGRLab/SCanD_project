27a8b985561af54b73a9a2ff9a2ec2e388dc22db
GhazalehManj <126309136+GhazalehManj@users.noreply.github.com>
2026-02-19 18:48:06 -0500

    Update 02_fmriprep_apply_scinet.sh

--- code/02_fmriprep_apply_scinet.sh
+++ code/02_fmriprep_apply_scinet.sh
@@ -65,28 +65,6 @@ export APPTAINERENV_FS_LICENSE=/home/fmriprep/.freesurfer.txt
 # done
 
 
-# --------------------------------------------
-# Detect functopup fieldmaps
-# --------------------------------------------
-HAS_FUNCTOPUP=0
-
-if find "${BIDS_DIR}/sub-${SUBJECTS}" \
-    \( -path "*/ses-*/fmap/*functopup*.nii.gz" -o -path "*/fmap/*functopup*.nii.gz" \) \
-    -print -quit | grep -q .; then
-    HAS_FUNCTOPUP=1
-fi
-
-SDC_ARGS="--use-syn-sdc"
-
-if [ "$HAS_FUNCTOPUP" -eq 0 ]; then
-    echo "No functopup found for sub-${SUBJECTS} → using SyN-SDC"
-    SDC_ARGS="--use-syn-sdc warn --force syn-sdc"
-else
-    echo "functopup found for sub-${SUBJECTS} → NOT forcing SyN"
-    SDC_ARGS="--use-syn-sdc warn"
-fi
-
-echo "SDC flags: ${SDC_ARGS}"
 
 
 singularity run --cleanenv \
@@ -100,10 +78,10 @@ singularity run --cleanenv \
     --derivatives /derived \
     -w /work \
     --skip-bids-validation \
+    --use-syn-sdc \
     --cifti-output 91k \
     --ignore slicetiming \
-    --level full \
-    ${SDC_ARGS}
+    --level full 
 
 
 # note, if you have top-up fieldmaps than you can uncomment the last two lines of the above script
