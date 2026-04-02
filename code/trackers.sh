c1c0d379a562f6eaf37297eb543581b6d92d7423
GhazalehManj <126309136+GhazalehManj@users.noreply.github.com>
2026-04-02 12:14:21 -0400

    Update trackers.sh

--- code/trackers.sh
+++ code/trackers.sh
@@ -157,7 +157,10 @@ singularity exec \
         --pipeline fmriprepapply \
         --pipeline-version 25.2.4 \
   '
-
+  
+python "$BASEDIR/code/fmriprep_method_tsv.py" \
+  --fmriprep-root "$BASEDIR/data/local/derivatives/fmriprep/25.2.4" \
+  --output-tsv "$BASEDIR/Neurobagel/derivatives/processing_status_fmriprep.tsv"
 
 singularity exec \
   --env BASEDIR="$BASEDIR" \
