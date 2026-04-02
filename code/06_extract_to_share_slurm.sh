37705896364ad8a6815a146fab14953272d433d4
GhazalehManj <126309136+GhazalehManj@users.noreply.github.com>
2026-04-02 12:11:09 -0400

    Update 06_extract_to_share_slurm.sh

--- code/06_extract_to_share_slurm.sh
+++ code/06_extract_to_share_slurm.sh
@@ -393,6 +393,7 @@ fi
 # sharing nipoppy trackers
 cp "$(ls -t ${PROJECT_DIR}/Neurobagel/derivatives/.processing_statuses/processing_status-*.tsv | head -n 1)" data/share/processing_status.tsv
 cp "$(ls -t ${BASEDIR}/Neurobagel/.manifests/manifest*.tsv | head -n 1)" data/share/manifest.tsv
+cp ${BASEDIR}/Neurobagel/derivatives/processing_status_fmriprep.tsv  ${BASEDIR}/data/share
 
 cp ${PROJECT_DIR}/data/local/bids/participants.tsv ${PROJECT_DIR}/data/share
 
