787a13b73e212356f68fd8925e65e3fd5423603c
Ghazaleh Mohammadalinejad <ghazalehmohammadalinejad@Ghazalehs-MacBook-Pro.local>
2026-06-02 16:46:26 -0400

    Revert amico VNC work dir change; keep participant label fix.
    Leave WORK_DIR as SLURM_TMPDIR/SCanD/amico unchanged from trillium/Fir base.

--- code/03_amico_VNC.sh
+++ code/03_amico_VNC.sh
@@ -9,7 +9,7 @@ export QSIPREP_DIR=${BASEDIR}/data/local/derivatives/qsiprep/0.22.0/qsiprep
 export SING_CONTAINER=${BASEDIR}/containers/qsiprep-0.22.0.sif
 export OUTPUT_DIR=${BASEDIR}/data/local/derivatives/qsiprep/0.22.0/amico_noddi
 export TMP_DIR=${BASEDIR}/data/local/derivatives/qsiprep/0.22.0/amico_noddi/tmp
-export WORK_DIR=${SLURM_TMPDIR:-${TMPDIR:-/tmp}}/SCanD/amico
+export WORK_DIR=${SLURM_TMPDIR}/SCanD/amico
 export LOGS_DIR=${BASEDIR}/logs
 export SINGULARITYENV_FS_LICENSE=${BASEDIR}/templates/.freesurfer.txt
 
